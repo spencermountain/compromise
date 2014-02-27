ef6e041b1b3a877d68be7162ebbee14071b03239
Spencer Kelly <spencermountain@gmail.com>
2014-02-26 23:51:15 -0500

    pluralize singularize in same method

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-// _ = require("underscore")
 exports.sentences = require('./tokenization/sentence').sentences;
-// exports.pluralize = require('./inflection/pluralize').pluralize;
-exports.singularize = require('./inflection/singularize');
-exports.tagger = require('./inflection/singularize');
+exports.pluralize = require('./inflection/inflect').pluralize;
+exports.singularize = require('./inflection/inflect').singularize;
 exports.adj_to_noun = require('./conjugation/adj_to_noun').adj_to_noun;
 exports.tag = require('./tagging/tagger');
 exports.dates = require('./spotting/date_parser');
 
 var spot_function = require('./spotting/spotter').spot;
 exports.spot = function(text) {
-    var tags = exports.tag(text)
-    return spot_function(tags)
+	var tags = exports.tag(text)
+	return spot_function(tags)
 }

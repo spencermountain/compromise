fc4ab44f6079e67618fd99fa4b2080865d12b327
Spencer Kelly <spencermountain@gmail.com>
2014-06-04 09:32:16 -0400

    improve adjective conjugations, lexicon

--- src/parents/adjective/conjugate/to_comparative.js
+++ src/parents/adjective/conjugate/to_comparative.js
@@ -4,25 +4,90 @@ var to_comparative = (function() {
 
 
 		var irregulars = {
-			"poor": "poorer",
-			"pure": "purer",
-			"brown": "browner",
-			"orange": "oranger",
+			"grey": "greyer",
+			"gray": "grayer",
 			"green": "greener",
 			"yellow": "yellower",
 			"red": "redder",
-			"blue": "bluer",
-			"black": "blacker",
-			"white": "whiter",
-			"wise": "wiser",
-			"round": "rounder",
-			"great": "greater",
-			"quiet": "quieter",
-			"quick": "quicker",
+			"good": "better",
+			"well": "better",
+		}
+		var dos = {
+			"black": 1,
+			"poor": 1,
+			"brown": 1,
+			"pure": 1,
+			"orange": 1,
+			"blue": 1,
+			"white": 1,
+			"wise": 1,
+			"large": 1,
+			"simple": 1,
+			"nice": 1,
+			"round": 1,
+			"great": 1,
+			"quiet": 1,
+			"quick": 1,
+			"weak": 1,
+			"clear": 1,
+			"firm": 1,
+			"young": 1,
+			"wide": 1,
+			"hard": 1,
+			"brave": 1,
+			"cool": 1,
+			"fair": 1,
+			"fresh": 1,
+			"grand": 1,
+			"sharp": 1,
+			"soft": 1,
+			"stiff": 1,
+			"thick": 1,
+			"thin": 1,
+			"warm": 1,
+			"wild": 1,
+			"new": 1,
+			"sweet": 1,
+
+			"formal": 1,
+			"raw": 1,
+			"weird": 1,
+			"broad": 1,
+			"fast": 1,
+			"glad": 1,
+			"odd": 1,
+			"mad": 1,
+			"strict": 1,
+			"tired": 1,
+			"solid": 1,
+			"smooth": 1,
+			"sad": 1,
+			"rich": 1,
+			"fine": 1,
+			"extreme": 1,
+			"rare": 1,
+			"safe": 1,
+			"mature": 1,
+			"lame": 1,
+			"feeble": 1,
+			"true": 1,
+			"free": 1,
+			"late": 1,
+			"clean": 1,
+			"short": 1,
+			"high": 1,
+			"dark": 1,
+
 		}
 		var dont = {
 			"overweight": 1,
-			"neat": 1
+			"neat": 1,
+			"main": 1,
+			"nearby": 1,
+			"asleep": 1,
+			"weekly": 1,
+			"secret": 1,
+			"certain": 1,
 		}
 		var transforms = [{
 			reg: /y$/i,
@@ -39,20 +104,30 @@ var to_comparative = (function() {
 		}, ]
 		var matches = [
 			/ght$/,
-			// /k$/,
 			/nge$/,
 			/ough$/,
 			/ain$/,
 			/uel$/,
 			/[au]ll$/,
 			/ow$/,
+			/old$/,
+			/oud$/,
+			/e[ae]p$/,
 		]
 		var not_matches = [
-			/ary$/
+			/ary$/,
+			/ous$/,
 		]
 
 		if (dont[str]) {
 			return null
+		}
+		if (dos[str]) {
+			if (str.match(/e$/)) {
+				return str + "r"
+			} else {
+				return str + "er"
+			}
 		}
 		if (irregulars[str]) {
 			return irregulars[str]
@@ -91,3 +166,4 @@ var to_comparative = (function() {
 // console.log(to_comparative('strange'))
 // console.log(to_comparative('narrow'))
 // console.log(to_comparative('dull'))
+// console.log(to_comparative('weak'))

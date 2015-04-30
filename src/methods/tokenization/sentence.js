d0220373a65b1b8153613e6bb9b46bfc3463d21a
Spencer Kelly <spencermountain@gmail.com>
2015-04-29 21:01:06 -0400

    elipsis support, and lower-case acronyms for sentence tokenization

--- src/methods/tokenization/sentence.js
+++ src/methods/tokenization/sentence.js
@@ -8,7 +8,7 @@ var sentence_parser = function(text) {
   //honourifics
   var abbrevs = ["jr", "mr", "mrs", "ms", "dr", "prof", "sr", "sen", "corp", "rep", "gov", "atty", "supt", "det", "rev", "col", "gen", "lt", "cmdr", "adm", "capt", "sgt", "cpl", "maj", "miss", "misses", "mister", "sir", "esq", "mstr", "phd", "adj", "adv", "asst", "bldg", "brig", "comdr", "hon", "messrs", "mlle", "mme", "op", "ord", "pvt", "reps", "res", "sens", "sfc", "surg"];
   //common abbreviations
-  abbrevs = abbrevs.concat(["arc", "al", "ave", "blvd", "cl", "ct", "cres", "exp", "rd", "st", "dist", "mt", "ft", "fy", "hwy", "la", "pd", "pl", "plz", "tce", "vs", "etc", "esp", "llb", "md", "bl", "ma", "ba", "lit", "fl", "ex", "eg", "i\.e", "e\.g"]);
+  abbrevs = abbrevs.concat(["arc", "al", "ave", "blvd", "cl", "ct", "cres", "exp", "rd", "st", "dist", "mt", "ft", "fy", "hwy", "la", "pd", "pl", "plz", "tce", "vs", "etc", "esp", "llb", "md", "bl", "ma", "ba", "lit", "fl", "ex", "eg", "ie"]);
   //place abbrevs
   abbrevs = abbrevs.concat(["ala", "ariz", "ark", "cal", "calif", "col", "colo", "conn", "del", "fed", "fla", "ga", "ida", "id", "ill", "ind", "ia", "kan", "kans", "ken", "ky", "la", "me", "md", "mass", "mich", "minn", "miss", "mo", "mont", "neb", "nebr", "nev", "mex", "okla", "ok", "ore", "penna", "penn", "pa", "dak", "tenn", "tex", "ut", "vt", "va", "wash", "wis", "wisc", "wy", "wyo", "usafa", "alta", "ont", "que", "sask", "yuk"]);
   //date abbrevs
@@ -20,7 +20,8 @@ var sentence_parser = function(text) {
 
   //detection of non-sentence chunks
   var abbrev_reg = new RegExp("(^| )(" + abbrevs.join("|") + ")[.!?] ?$", "i");
-  var acronym_reg= new RegExp("[ |\.][A-Z]\.?$")
+  var acronym_reg= new RegExp("[ |\.][A-Z]\.?$", "i")
+  var elipses_reg= new RegExp("\\.\\.\\.*$")
 
   //loop through these chunks, and join the non-sentence chunks back together..
   var chunks_length = chunks.length;
@@ -29,8 +30,8 @@ var sentence_parser = function(text) {
       //trim whitespace
       chunks[i] = chunks[i].replace(/^\s+|\s+$/g, "");
       //should this chunk be combined with the next one?
-      if (chunks[i+1] && chunks[i].match(abbrev_reg) || chunks[i].match(acronym_reg)) {
-          chunks[i + 1] = (chunks[i]||'') + " " + (chunks[i + 1]||'');
+      if (chunks[i+1] && chunks[i].match(abbrev_reg) || chunks[i].match(acronym_reg) || chunks[i].match(elipses_reg) ) {
+          chunks[i + 1] = ((chunks[i]||'') + " " + (chunks[i + 1]||'')).replace(/ +/g, " ");
       } else if(chunks[i] && chunks[i].length>0){ //this chunk is a proper sentence..
           sentences.push(chunks[i]);
           chunks[i] = "";
@@ -51,3 +52,4 @@ if (typeof module !== "undefined" && module.exports) {
 // console.log(sentence_parser('Tony is nice. He lives in Japan.').length === 2)
 // console.log(sentence_parser('I like that Color').length === 1)
 // console.log(sentence_parser("She was dead. He was ill.").length === 2)
+// console.log(sentence_parser("i think it is good ... or else.").length == 1)

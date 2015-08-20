ab86856c9e1edfc4fa4c36e8b7c11d4da420bc93
Rob Ellis <rob@chloi.io>
2015-08-19 21:27:24 -0700

    update lint workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,21 +7,30 @@ module.exports = function(grunt) {
       tasks: ['run:build', 'run:run'],
     },
 
-    run: {
-      lint: {
-        exec: "tslint ./src"
+    tslint: {
+      options: {
+        configuration: grunt.file.readJSON("tslint.json")
       },
+      src: [
+        "src/*.ts",
+      ]
+    },
+    run: {
       build: {
-        exec: "tsc ./src/index.ts --module commonjs --target es5 --outDir ./build"
+        exec: 'tsc ./src/index.ts --module commonjs --target es5 --outDir ./build'
       },
       run: {
         exec: 'node ./build/index.js',
       }
     }
 
-  })
+  });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-run');
+  grunt.loadNpmTasks('grunt-tslint');
   grunt.registerTask('default', ['run:build', 'run:run', 'watch']);
+  grunt.registerTask('lint', ['tslint:src']);
+   
+  
 };

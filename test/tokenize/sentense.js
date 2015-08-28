3b29f3f8b4b0f80cbdbbd6a051cd3a92e9b57a2f
Rob Ellis <rob@chloi.io>
2015-08-27 21:55:34 -0700

    update tests to use es6

--- test/tokenize/sentense.js
+++ test/tokenize/sentense.js
@@ -1,14 +1,13 @@
-var mocha = require("mocha");
-var should = require("should");
+import mocha from "mocha";
+import should from "should";
+import Sentences from "../../src";
 
-var Sentences = require("../../build/tokenize/sentences");
-
-describe("tokenize sentense tests", function () {
-  it("tokenize basic sentence", function (done) {
-    Sentences("I like that Color").should.have.length(1);
-    Sentences("She was dead. He was ill.").should.have.length(2);
-    Sentences("i think it is good... or else.").should.have.length(1);
-    Sentences("Dr. Tony is nice. He lives on Elm St. in Vancouver BC. Canada.").should.have.length(2);
+describe("tokenize sentence tests", () => {
+  it("tokenize basic sentence", (done) => {
+    Sentences("I like that Color").sentences.should.have.length(1);
+    Sentences("She was dead. He was ill.").sentences.should.have.length(2);
+    Sentences("i think it is good... or else.").sentences.should.have.length(1);
+    Sentences("Dr. Tony is nice. He lives on Elm St. in Vancouver BC. Canada.").sentences.should.have.length(2);
     done();
   });
 });

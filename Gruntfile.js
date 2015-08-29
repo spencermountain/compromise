85602bdbb6c501721246c89d2211ab5499f23f5a
Rob Ellis <rob@chloi.io>
2015-08-28 20:14:40 -0700

    fixes tests again

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,6 +79,7 @@ module.exports = function(grunt) {
     mochaTest: {
       test: {
         options: {
+          require: "babel/register",
           reporter: "spec",
           clearRequireCache: true,
           colors: true,

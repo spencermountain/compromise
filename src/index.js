79c425c19464071c3d48ffe52d0e9a588ab45ec1
Spencer Kelly <spencermountain@gmail.com>
2015-09-03 23:17:40 -0400

    new api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 "use strict";
 const Text = require("./text/text.js");
+const Term = require("./term/term.js");
+const Verb = require("./term/verb/verb.js");
+const Noun = require("./term/noun/noun.js");
+const Value = require("./term/value/value.js");
+const Adjective = require("./term/adjective/adjective.js");
+const Adverb = require("./term/adverb/adverb.js");
 
-//by abstracting Text, we allow multiple Text objects by the same user
-const nlp = function(str) {
-  return new Text(str);
+//function returns a text object if there's a param, otherwise
+const Api = function(str) {
+  this.Term=function(s){
+    return new Term(s);
+  }
+  this.Verb=function(s){
+    return new Verb(s);
+  }
+  this.Adverb=function(s){
+    return new Adverb(s);
+  }
+  this.Noun=function(s){
+    return new Noun(s);
+  }
+  this.Adjective=function(s){
+    return new Adjective(s);
+  }
+  if(str){
+    return new Text(str);
+  }
 };
 
-module.exports = nlp;
+module.exports = Api;
 
-// console.log(nlp('John is pretty').parents())
+//return a Text object..
+// let nlp1=new Api("john is cool")
+// console.log(nlp1.text())
+
+//return a Term/Value object
+// let nlp2 = new Api()
+// console.log(nlp2.Term("john"))
+// console.log(nlp2.Verb("walk").conjugate())
+

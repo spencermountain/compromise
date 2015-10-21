a0488d8178ba5b007ed118a8a2df0c3f1484adf5
Spencer Kelly <spencermountain@gmail.com>
2015-10-21 13:13:19 -0400

    more single quotes

--- src/data/abbreviations.js
+++ src/data/abbreviations.js
@@ -1,17 +1,17 @@
 //these are common word shortenings used in the lexicon and sentence segmentation methods
 //there are all nouns, or at the least, belong beside one.
-"use strict";
-const honourifics = require("./honourifics"); //stored seperately, for 'noun.is_person()'
+'use strict';
+const honourifics = require('./honourifics'); //stored seperately, for 'noun.is_person()'
 
 let main = [
   //common abbreviations
-  "arc", "al", "ave", "blvd", "cl", "ct", "cres", "exp", "rd", "st", "dist", "mt", "fy", "hwy", "pd", "pl", "plz", "tce", "llb", "md", "bl", "ma", "ba", "lit",
+  'arc', 'al', 'ave', 'blvd', 'cl', 'ct', 'cres', 'exp', 'rd', 'st', 'dist', 'mt', 'fy', 'hwy', 'pd', 'pl', 'plz', 'tce', 'llb', 'md', 'bl', 'ma', 'ba', 'lit',
   //place main
-  "ala", "ariz", "ark", "cal", "calif", "col", "colo", "conn", "del", "fed", "fla", "fl", "ga", "ida", "ind", "ia", "la", "kan", "kans", "ken", "ky", "la", "md", "mich", "minn", "mont", "neb", "nebr", "nev", "okla", "penna", "penn", "pa", "dak", "tenn", "tex", "ut", "vt", "va", "wash", "wis", "wisc", "wy", "wyo", "usafa", "alta", "ont", "que", "sask", "yuk",
+  'ala', 'ariz', 'ark', 'cal', 'calif', 'col', 'colo', 'conn', 'del', 'fed', 'fla', 'fl', 'ga', 'ida', 'ind', 'ia', 'la', 'kan', 'kans', 'ken', 'ky', 'la', 'md', 'mich', 'minn', 'mont', 'neb', 'nebr', 'nev', 'okla', 'penna', 'penn', 'pa', 'dak', 'tenn', 'tex', 'ut', 'vt', 'va', 'wash', 'wis', 'wisc', 'wy', 'wyo', 'usafa', 'alta', 'ont', 'que', 'sask', 'yuk',
   //org main
-  "dept", "univ", "assn", "bros", "inc", "ltd", "co", "corp",
+  'dept', 'univ', 'assn', 'bros', 'inc', 'ltd', 'co', 'corp',
   //proper nouns with exclamation marks
-  "yahoo", "joomla", "jeopardy"
+  'yahoo', 'joomla', 'jeopardy'
 ];
 //person titles like 'jr', (stored seperately)
 main = main.concat(honourifics);

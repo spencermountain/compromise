3d243625e077adcdd11a515cd4f936499fd7c39f
Spencer Kelly <spencermountain@gmail.com>
2016-05-31 16:33:32 -0400

    build for release

--- src/index.js
+++ src/index.js
@@ -101,4 +101,4 @@ if (typeof define === 'function' && define.amd) {
 // console.log(nlp.text(' \n\n\t spencer is here\n\nhe is here too'));
 // console.log(nlp.sentence(`john's good`).contractions.expand().terms);
 // console.log(nlp.sentence(`everyone's here`).contractions.expand().text());
-// console.log(nlp.sentence(`Toronto's citizens`).terms);
+// console.log(nlp.text(`Jon doe is a geek`).people());
--- src/term/noun/value/value.js
+++ src/term/noun/value/value.js
@@ -177,7 +177,7 @@ class Value extends Noun {
     this.number = to_number(numbers);
 
     //of_what
-    var of_pos = this.text.indexOf(' of ');
+    let of_pos = this.text.indexOf(' of ');
     if (of_pos > 0) {
       this.of_what = this.text.substring(of_pos + 4).trim();
     } else if (this.unit_name) {

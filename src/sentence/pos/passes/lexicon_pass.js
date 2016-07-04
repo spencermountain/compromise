a0017e1ada112d3e69493e0df512700fc6f3a4c8
Spencer Kelly <spencermountain@gmail.com>
2016-07-04 19:07:23 -0400

    fix suffix lexicon bug

--- src/sentence/pos/passes/lexicon_pass.js
+++ src/sentence/pos/passes/lexicon_pass.js
@@ -32,8 +32,10 @@ const lexicon_pass = function(terms, options) {
     //try to match without a contraction - "they've" -> "they"
     if (t.has_abbreviation()) {
       let attempt = normal.replace(/'(ll|re|ve|re|d|m|s)$/, '');
-      // attempt = normal.replace(/n't/, '');
-      return assign(t, lexicon[attempt], 'lexicon_prefix');
+      // attempt = attempt.replace(/n't/, '');
+      if (lexicon[attempt]) {
+        return assign(t, lexicon[attempt], 'lexicon_suffix');
+      }
     }
 
     //match 'twenty-eight'

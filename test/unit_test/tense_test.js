ca3270f3e8edd6f43d6cf29fbe4f9c2e7778f38f
Spencer Kelly <spencermountain@gmail.com>
2016-07-04 14:52:30 -0400

    finished moving tests, bump version

--- test/unit_test/tense_test.js
+++ test/unit_test/tense_test.js
@@ -12,12 +12,19 @@ test('sentence-change-tense:', function(t) {
     // ['cardboard is made of tree fibre', 'cardboard was made of tree fibre', 'cardboard will be made of tree fibre'],
     ['he finishes first', 'he finished first', 'he will finish first'],
     ['our house looks great', 'our house looked great', 'our house will look great'],
+
     //infinitives
     ['he does what he can to stop', 'he did what he could to stop', 'he will do what he can to stop'],
     ['goes to sleep', 'went to sleep', 'will go to sleep'],
+
     //grammatical-number
     // ['we do what we can to stop', 'we did what we could to stop', 'we will do what we can to stop'],
 
+    //multi-sentence
+    ['this is one sentence. This makes two now.', 'this was one sentence. This made two now.', 'this will be one sentence. This will make two now.'],
+
+  //support negative
+  // ['this isn\'t one sentence. This doesn\'t make two now.', 'this was not one sentence. This didn\'t make two now.', 'this won\'t be one sentence. This won\'t make two now.']
   ].forEach(function (a) {
     var s = nlp.text(a[0]);
 

ef90f9dc715775ccf493f84e53cabd853138f957
Spencer Kelly <spencermountain@gmail.com>
2016-10-06 10:00:49 -0400

    ngram working

--- src/models/result/methods/ngram.js
+++ src/models/result/methods/ngram.js
@@ -1,37 +1,55 @@
 'use strict';
-
-//do either bigram, trigram, whatever,,
-const doSize = (size, terms) => {
-  let all = [];
-  for(let i = 0; i < terms.length - size + 1; i++) {
-    all.push();
-  }
-  return all;
-};
-
-//
+//ngrams are consecutive terms of a specific size
 const ngram = function(options) {
   options = options || {};
-  const maxGram = options.max_gram || 5;
-  const minGram = options.min_gram || 1;
-
+  options.size = options.size || [1, 2, 3];
+  if (typeof options.size === 'number') {
+    options.size = [options.size];
+  }
+  //flatten terms
   let terms = this.list.map((ts) => {
     return ts.terms.map((t) => t.normal);
   });
+
+  //count freq
   let obj = {};
   //each gram-size
-  for(let size = minGram; size < maxGram; size++) {
+  options.size.forEach((size) => {
+    obj[size] = {};
     //each sentence/match
     for(let s = 0; s < terms.length; s++) {
-      // all = all.concat(doSize(size, terms[s]));
-      for(let o = 0; o < terms[s].length; o++) {
+      //start slice at each term
+      for(let o = 0; o < terms[s].length - size + 1; o++) {
         let str = terms[s].slice(o, o + size).join(' ');
-        obj[str] = obj[str] || 0;
-        obj[str] += 1;
+        obj[size][str] = obj[size][str] || 0;
+        obj[size][str] += 1;
       }
     }
-  }
-  return obj;
+  });
+
+  //flatten to an array
+  let arr = [];
+  Object.keys(obj).forEach((size) => {
+    Object.keys(obj[size]).forEach((k) => {
+      arr.push({
+        text: k,
+        count: obj[size][k],
+        size: parseInt(size, 10)
+      });
+    });
+  });
+  //sort the array
+  arr = arr.sort((a, b) => {
+    if (a.count > b.count) {
+      return -1;
+    }
+    //(the tie-braker)
+    if (a.count === b.count && a.size > b.size) {
+      return -1;
+    }
+    return 1;
+  });
+  return arr;
 };
 
 module.exports = ngram;

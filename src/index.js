ccdcce11bd7b700b6e5f64098fd74d0ee5b58eb0
spencermountain <spencermountain@gmail.com>
2016-11-18 15:59:33 -0500

    add version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const Result = require('./result');
+const pkg = require('../package.json');
 
 const nlp = function (str, context) {
   return Result.fromString(str, context);
 };
+nlp.version = pkg.version;
 
 module.exports = nlp;

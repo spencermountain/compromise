5a4240f6fcb17b91375dd878bf430dc2a2d37d5e
spencermountain <spencermountain@gmail.com>
2016-12-13 11:11:32 -0500

    tests passing

--- test/unit/match/remove.test.js
+++ test/unit/match/remove.test.js
@@ -3,16 +3,16 @@ var nlp = require('../lib/nlp');
 
 test('remove-basic :', function(t) {
 
-  var m = nlp('the brown cat played').match('brown').remove();
+  var m = nlp('the brown cat played').match('brown').delete();
   t.equal(m.plaintext(), 'the cat played', 'brown-cat');
 
-  m = nlp('the nice brown cat played').match('nice brown').remove();
+  m = nlp('the nice brown cat played').match('nice brown').delete();
   t.equal(m.plaintext(), 'the cat played', 'nice-brown');
 
-  m = nlp('the nice brown cat played').match('#Adjective').remove();
+  m = nlp('the nice brown cat played').match('#Adjective').delete();
   t.equal(m.plaintext(), 'the cat played', 'adj-each');
 
-  m = nlp('the nice brown cat played').match('#Adjective+').remove();
+  m = nlp('the nice brown cat played').match('#Adjective+').delete();
   t.equal(m.plaintext(), 'the cat played', 'adj-consecutive');
 
   t.end();
@@ -20,32 +20,32 @@ test('remove-basic :', function(t) {
 
 test('remove-match :', function(t) {
 
-  var m = nlp('the brown cat played').remove('brown');
+  var m = nlp('the brown cat played').delete('brown');
   t.equal(m.plaintext(), 'the cat played', 'brown-cat');
 
-  m = nlp('the brown cat played. The brown dog sat down.').remove('brown');
+  m = nlp('the brown cat played. The brown dog sat down.').delete('brown');
   t.equal(m.plaintext(), 'the cat played. The dog sat down.', 'brown-cat');
 
-  m = nlp('the nice brown cat played. The nice dog waited.').remove('nice brown');
+  m = nlp('the nice brown cat played. The nice dog waited.').delete('nice brown');
   t.equal(m.plaintext(), 'the cat played. The nice dog waited.', 'nice-brown');
 
-  m = nlp('the nice brown cat played. The cute dogs ate.').remove('#Adjective');
+  m = nlp('the nice brown cat played. The cute dogs ate.').delete('#Adjective');
   t.equal(m.plaintext(), 'the cat played. The dogs ate.', 'adj-each');
 
-  m = nlp('the nice brown cat played. The cute dogs ate.').remove('#Adjective+');
+  m = nlp('the nice brown cat played. The cute dogs ate.').delete('#Adjective+');
   t.equal(m.plaintext(), 'the cat played. The dogs ate.', 'adj-consecutive');
 
   t.end();
 });
 
 test('remove-logic :', function(t) {
-  var m = nlp('spencer kelly is here').match('spencer kelly').remove('spencer');
+  var m = nlp('spencer kelly is here').match('spencer kelly').delete('spencer');
   t.equal(m.normal(), 'kelly', 'remove(reg) returns this');
 
-  m = nlp('spencer kelly is here').match('spencer kelly').remove();
+  m = nlp('spencer kelly is here').match('spencer kelly').delete();
   t.equal(m.normal(), 'is here', 'remove() returns parent');
 
-  m = nlp('spencer kelly is here').match('spencer kelly').remove('notfound');
+  m = nlp('spencer kelly is here').match('spencer kelly').delete('notfound');
   t.equal(m.normal(), 'spencer kelly', 'remove(notfound) returns this');
   t.end();
 });

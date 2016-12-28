f451dce601600d982c02a3f18c83a8482c31ed83
Spencer Kelly <spencermountain@gmail.com>
2016-12-28 13:47:41 -0500

    yolo

--- test/unit/match/replace.test.js
+++ test/unit/match/replace.test.js
@@ -21,16 +21,17 @@ test('replace-basic :', function(t) {
   t.end();
 });
 
-// test('match-replace :', function(t) {
-//   [
-//     ['the dog played', 'the dog', 'the cat', 'the cat played'],
-//     ['the dog played', 'the #Noun', 'the cat', 'the cat played'],
-//     ['the dog played', 'the (dog|hamster|pet-snake)', 'the cat', 'the cat played'],
-//     ['the boy and the girl', 'the #Noun#', 'the house', 'the house and the house'],
-//     ['the boy and the girl', 'the cat', 'the house', 'the boy and the girl'],
-//   ].forEach(function(a) {
-//     var str = nlp(a[0]).replace(a[1], a[2]).plaintext();
-//     str_test(str, a[0], a[3], t);
-//   });
-//   t.end();
-// });
+test('match-replace :', function(t) {
+  [
+    ['the dog played', 'the dog', 'the cat', 'the cat played'],
+    ['the dog played', 'the #Noun', 'the cat', 'the cat played'],
+    ['the dog played', 'the (dog|hamster|pet-snake)', 'the cat', 'the cat played'],
+    ['the boy and the girl', 'the #Noun', 'the house', 'the house and the house'],
+    ['the boy and the girl', 'the cat', 'the house', 'the boy and the girl'],
+  ].forEach(function(a) {
+    var str = nlp(a[0]).replace(a[1], a[2]).plaintext();
+    var msg = str + ' -- ' + a[3];
+    t.equal(str, a[3], msg);
+  });
+  t.end();
+});

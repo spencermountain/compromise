72562bbed8dc044d3c791e749de70d0b35eee388
spencermountain <spencermountain@gmail.com>
2017-02-10 13:24:18 -0500

    keep spliton

--- test/unit/transform/split.test.js
+++ test/unit/transform/split.test.js
@@ -50,17 +50,17 @@ test('splitBefore', function (t) {
 
 test('splitOn', function (t) {
   [
-    ['doug and nancy', 'and', ['doug', 'nancy']],
-    ['doug and also nancy', 'and also', ['doug', 'nancy']],
-    ['doug and definetly nancy', 'and #Adverb', ['doug', 'nancy']],
-    ['maybe doug but possibly nancy', 'but', ['maybe doug', 'possibly nancy']],
-    ['doug is really nice', 'is', ['doug', 'really nice']],
+    ['doug and nancy', 'and', ['doug', 'and', 'nancy']],
+    ['doug and also nancy', 'and also', ['doug', 'and also', 'nancy']],
+    ['doug and definetly nancy', 'and #Adverb', ['doug', 'and definetly', 'nancy']],
+    ['maybe doug but possibly nancy', 'but', ['maybe doug', 'but', 'possibly nancy']],
+    ['doug is really nice', 'is', ['doug', 'is', 'really nice']],
 
-    ['a x b x c', 'x', ['a', 'b', 'c']],
-    ['a b x x c', 'x', ['a b', 'c']],
-    ['x a b x c', 'x', ['a b', 'c']],
-    ['x x a b c', 'x', ['a b c']],
-    ['a x b x', 'x', ['a', 'b']],
+    ['a x b x c', 'x', ['a', 'x', 'b', 'x', 'c']],
+    ['a b x x c', 'x', ['a b', 'x', 'x', 'c']],
+    ['x a b x c', 'x', ['x', 'a b', 'x', 'c']],
+    ['x x a b c', 'x', ['x', 'x', 'a b c']],
+    ['a x b x', 'x', ['a', 'x', 'b', 'x']],
   ].forEach(function (a) {
     var want = a[2];
     var got = nlp(a[0]).splitOn(a[1]).out('array');

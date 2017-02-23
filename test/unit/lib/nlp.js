66d8ec863b36a30e12224cbb94a6b43d29f65e59
spencermountain <spencermountain@gmail.com>
2017-02-23 11:24:48 -0500

    use build file for tests

--- test/unit/lib/nlp.js
+++ test/unit/lib/nlp.js
@@ -1,7 +1,7 @@
 var nlp;
 if (typeof window !== undefined) {
-  nlp = require('../../../src/index');
-// nlp = require('../../../builds/compromise');
+  // nlp = require('../../../src/index');
+  nlp = require('../../../builds/compromise');
 } else {
   nlp = window.nlp;
   alert('browser');

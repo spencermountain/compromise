ca93b416c8db7b803bf13d85c8d0f85c56a1584f
spencermountain <spencermountain@gmail.com>
2017-02-23 13:21:47 -0500

    demos and tests for ie9

--- scripts/browserTest.js
+++ scripts/browserTest.js
@@ -4,9 +4,17 @@ var fileServer = '"./node_modules/.bin/http-server"';
 
 //run tests on the client-side
 var out = './test/client/compiled_tests.js';
-var cmd = browserifyGlob + ' "./test/unit/**/*.test.js" > ' + out;
+var cmd = browserifyGlob + ' "./test/unit/**/*.test.js" ';
+cmd += ' -t [ babelify --presets [ es2015 stage-2 ] ] > ' + out;
 cmd += ' && ' + fileServer + ' test/client -o -c-1';
 exec(cmd);
 
 //then cleanup that god-awful file in our sourcecode
 // exec('rm ' + out);
+
+
+//es5 main (browserify + derequire)
+// cmd = lib.browserify + ' "./src/index.js" --standalone nlp';
+// cmd += ' | ' + lib.derequire;
+// cmd += ' >> ' + path.es5;
+// exec(cmd);

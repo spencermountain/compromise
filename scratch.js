864ff79ce75aa66b423b94a857a1b0f67f4cb5ba
spencermountain <spencermountain@gmail.com>
2017-03-05 15:14:58 -0500

    scale-back date parsing

--- scratch.js
+++ scratch.js
@@ -1,8 +1,8 @@
 'use strict';
 //this file is not included in the build.
 //use it for messing around.
-// var nlp = require('./src/index');
-var nlp = require('./builds/compromise');
+var nlp = require('./src/index');
+// var nlp = require('./builds/compromise');
 // const corpus = require('nlp-corpus');
 // let sotu = corpus.sotu.parsed()[23];
 // const fresh = require('./test/unit/lib/freshPrince.js');
@@ -21,5 +21,6 @@ var nlp = require('./builds/compromise');
 // };
 // console.log(nlp('it is mother of pearl', lexicon, tagSet).terms().last().out('tags'));
 
-var r = nlp('Larry, Curly, and Moe').terms().people().sort();
-console.log(r.out('array'));
+var r = nlp('on June 5th 1992. thursday September 9th 2016 at 3am. 2018/03/28').dates();
+console.log(r.data()[1]);
+// console.log(r.out('array'));

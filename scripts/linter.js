e662cf67c10664c0ac1b22027162c6c24c01f822
spencermountain <spencermountain@gmail.com>
2017-03-13 19:09:58 -0400

    better prepublish

--- scripts/linter.js
+++
@@ -1,9 +0,0 @@
-require('shelljs/global');
-config.silent = true;
-
-var eslint = '"node_modules/.bin/eslint"';
-
-//run linter
-exec(eslint + ' -c .eslintrc --color "./src/**"', {
-  async: true
-});

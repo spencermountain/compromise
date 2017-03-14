5cfa2eb1493504be463ea45f46a57d592e0dc81d
spencermountain <spencermountain@gmail.com>
2017-03-14 12:54:51 -0400

    use unpkg to host lib #251

--- demo/webworker/webworker.js
+++ demo/webworker/webworker.js
@@ -1,6 +1,6 @@
 //loads and runs compromise inside the worker-instance
 self.addEventListener('message', function(e) {
-  importScripts('../builds/compromise.js');
+  importScripts('https://unpkg.com/compromise@latest/builds/compromise.min.js');
   var r = self.nlp(e.data);
   var s = r.sentences(0).toUpperCase();
   self.postMessage(s.out('html'));

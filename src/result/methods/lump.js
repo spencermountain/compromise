4e7d2f11c0efc412dc2804c7a6e053003c263a75
leoseccia <leo@materialiser.co.uk>
2017-03-17 11:56:59 +0000

    changed lump to return parent with lumped terms

--- src/result/methods/lump.js
+++ src/result/methods/lump.js
@@ -7,6 +7,23 @@ const lumpMethods = (Text) => {
     lump: function () {
       this.list.forEach((ts) => { ts.lump(); });
       return this;
+    },
+    
+    lumpIntoParent: function(tags, description)
+    {
+      for(let mI = this.list.length-1; mI >= 0; mI--){
+        let firstTerm = this.list[mI].firstTerm();
+        let lastTerm = this.list[mI].lastTerm();
+
+        this.parent.lumpIntoOne(firstTerm, lastTerm, tags, description);
+
+        // let lumpedTerm = this.list[mI].lump(); 
+        // lumpedTerm.tagAs(tags, description);
+
+        
+      }
+
+      return this.parent;
     }
   };
 

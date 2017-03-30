afe13b56555656b44b353dc0473875147e2cd495
spencermountain <spencermountain@gmail.com>
2017-03-30 15:40:48 -0400

    yolo

--- scratch.js
+++ scratch.js
@@ -5,7 +5,7 @@ var nlp = require('./src/index');
 // nlp.verbose('tagger');
 // const corpus = require('nlp-corpus');
 // let sotu = corpus.sotu.parsed()[23];
-// const fresh = require('./test/unit/lib/freshPrince.js');
+const fresh = require('./test/unit/lib/freshPrince.js');
 
 // bug.1
 //  .? vs *
@@ -15,18 +15,18 @@ var nlp = require('./src/index');
 // nlp('I\'m going to the shops').sentences().toPastTense().debug();
 
 
-let r = nlp.tokenize('5th - 7th').tag('NumberRange').debug();
+// let r = nlp.tokenize('5th - 7th').tag('NumberRange').debug();
 // r.contractions().debug();
 
 // //===timer
-// console.time('parse');
-// let r = nlp(fresh);
-// console.timeEnd('parse');
-//
-// console.time('match');
-// r.match('#Determiner (story|thing|#Adjective)', true);
-// console.timeEnd('match');
-//
-// console.time('tag');
-// r.tag('#Person');
-// console.timeEnd('tag');
+console.time('parse');
+let r = nlp(fresh);
+console.timeEnd('parse');
+
+console.time('match');
+r.match('#Determiner (story|thing|#Adjective)', true);
+console.timeEnd('match');
+
+console.time('tag');
+r.tag('#Person');
+console.timeEnd('tag');

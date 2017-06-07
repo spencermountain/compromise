8f8c3b346ae66a268070003d22f636e83162c82b
spencermountain <spencermountain@gmail.com>
2017-06-06 21:42:03 -0400

    test infinitive loops

--- scratch.js
+++ scratch.js
@@ -3,12 +3,9 @@ var nlp = require('./src/index');
 // nlp.verbose('tagger');
 // const fresh = require('./test/unit/lib/freshPrince.js');
 
-// var r = nlp('And you are?. Marshal');
-// r.sentences(); //.out();
-// r.debug();
-
-// var r2 = nlp(`- where is she.Oh.  you guys don't know?`).normalize().sentences().out();
-// r2.debug();
-
-var doc = nlp('are?.').not('are?');
-doc.debug();
+var lexicon = {
+  dorritos: 'chip'
+};
+var r = nlp('blend 2 tbsp of dorritos', lexicon);
+r.match('#Value #Unit of #chip').debug();
+console.log('hi');

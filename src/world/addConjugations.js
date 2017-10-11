0febca2d08b6d60ea4b8e184e23bd6a90694544f
spencermountain <spencermountain@gmail.com>
2017-10-11 19:38:59 -0400

    172 failing with smarter addconjugation

--- src/world/addConjugations.js
+++ src/world/addConjugations.js
@@ -3,17 +3,17 @@
 const addConjugations = function(obj) {
   Object.keys(obj).forEach((inf) => {
     this.conjugations[inf] = this.conjugations[inf] || {};
-  //add it to the lexicon
-  // this.words[inf] = this.words[inf] || 'Infinitive';
-  // Object.keys(obj[inf]).forEach((tag) => {
-  //   let word = obj[inf][tag];
-  //   //add this to our conjugations
-  //   this.conjugations[inf][tag] = word;
-  //   //add it to the lexicon, too
-  //   this.words[word] = this.words[word] || tag;
-  //   //also denormalize to cache.toInfinitive
-  //   this.cache.toInfinitive[obj[inf][tag]] = inf;
-  // });
+    //add it to the lexicon
+    this.words[inf] = this.words[inf] || 'Infinitive';
+    Object.keys(obj[inf]).forEach((tag) => {
+      let word = obj[inf][tag];
+      //add this to our conjugations
+      this.conjugations[inf][tag] = word;
+      //add it to the lexicon, too
+      this.words[word] = this.words[word] || tag;
+      //also denormalize to cache.toInfinitive
+      this.cache.toInfinitive[obj[inf][tag]] = inf;
+    });
   });
   return obj;
 };

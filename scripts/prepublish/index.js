17be3286f1a296fa6227443606bb7e6d9c5ab65e
spencermountain <spencermountain@gmail.com>
2017-10-18 10:55:00 -0400

    fix branch detection warning

--- scripts/prepublish/index.js
+++ scripts/prepublish/index.js
@@ -4,6 +4,7 @@ var chalk = require('chalk');
 
 console.log(chalk.green('\n==checking branch=='));
 var branch = exec('git status | grep "On branch" | cut -c 11-').stdout;
+branch = branch.trim();
 if (branch !== 'master') {
   console.log(chalk.red('\n\n   ============ hey, not on master branch ===========\n\n'));
 }
@@ -13,7 +14,7 @@ console.log(chalk.green('\n==sanity-test builds=='));
 var libs = [
   require('../../builds/compromise.js'),
   require('../../builds/compromise.min.js')
-  // require('../../builds/compromise.es6.min.js')
+// require('../../builds/compromise.es6.min.js')
 ];
 libs.forEach(nlp => {
   var r = nlp('John and Joe walked to the store');

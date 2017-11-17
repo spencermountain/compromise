322a019e0dfc86ef4ebba9d5026a57bd95f9dfbc
spencermountain <spencermountain@gmail.com>
2017-11-17 12:05:47 -0500

    use tap-dot

--- scripts/test.js
+++ scripts/test.js
@@ -1,10 +1,11 @@
 var exec = require('shelljs').exec;
 var tape = '"node_modules/.bin/tape"';
-var tapSpec = '"node_modules/.bin/tap-spec" --color';
+// var tapSpec = '"node_modules/.bin/tap-spec" --color';
+var tapDot = '"node_modules/.bin/tap-dot"';
 // var tapMin = '"node_modules/.bin/tap-min" --color';
 
 //the quotations here are strangely-important
-exec(tape + ' "./test/unit/**/*.test.js" | ' + tapSpec);
+exec(tape + ' "./test/unit/**/*.test.js" | ' + tapDot);
 
 // var tapMin = '"node_modules/.bin/tap-min" --color';
 // exec(tape + ' "./test/unit/**/*.test.js" | ' + tapMin);

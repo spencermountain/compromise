0f4031c52f3a908bb97ac125266040c261aa0178
Sca <scagood@me.com>
2018-02-27 14:54:25 +0000

    regex whack-a-mole

--- src/text/tokenize.js
+++ src/text/tokenize.js
@@ -2,20 +2,30 @@
 // Ignore periods/questions/exclamations used in acronyms/abbreviations/numbers, etc.
 // @spencermountain 2017 MIT
 'use strict';
-const abbreviations = Object.keys(require('../world/more-data/abbreviations'));
+const abbreviations = Object.keys(require('./abbreviations'));
+// \u203D - INTERROBANG
+// \u2E18 - INVERTED INTERROBANG
+// \u203C - DOUBLE EXCLAMATION MARK
+// \u2047 - DOUBLE QUESTION MARK
+// \u2048 - QUESTION EXCLAMATION MARK
+// \u2049 - EXCLAMATION QUESTION MARK
+
 //regs-
-const abbrev_reg = new RegExp('\\b(' + abbreviations.join('|') + ')[.!?] ?$', 'i');
-const acronym_reg = new RegExp('[ |.][A-Z].?( *)$', 'i');
-const elipses_reg = new RegExp('\\.\\.+( *)$');
+const abbrev_reg = new RegExp('(?:' + abbreviations.join('|') + ')[.!?]$', 'i');
+const acronym_reg = /[a-z](?:\.[a-z])*\.?$/i;
+const elipses_reg = /(?:\u2026|\.{2,})$/;
+
+const line_reg = /(?:\r?\n|\r)+/;
+const naiive_sentence_reg = /[^.!?\u203D\u2E18\u203C\u2047-\u2049]+(?:[.!?\u203D\u2E18\u203C\u2047-\u2049]+|$)/g;
 
 //start with a regex:
 const naiive_split = function(text) {
   let all = [];
-  //first, split by newline
-  let lines = text.split(/(\n+)/);
+  // Split by the majority of new line variations.
+  let lines = text.split(line_reg);
   for (let i = 0; i < lines.length; i++) {
-    //split by period, question-mark, and exclamation-mark
-    let arr = lines[i].split(/(\S.+?[.!?])(?=\s+|$)/g);
+    // Split by period, question-mark, and exclamation-mark
+    let arr = lines[i].match(naiive_sentence_reg);
     for (let o = 0; o < arr.length; o++) {
       all.push(arr[o]);
     }
@@ -62,9 +72,17 @@ const sentence_parser = function(text) {
   for (let i = 0; i < chunks.length; i++) {
     let c = chunks[i];
     //should this chunk be combined with the next one?
-    if (chunks[i + 1] && /[a-zA-Z]/.test(c) && (abbrev_reg.test(c) || acronym_reg.test(c) || elipses_reg.test(c))) {
+    if (
+      chunks[i + 1] &&
+      /[a-z]/i.test(c) &&
+      (
+        abbrev_reg.test(c) ||
+        acronym_reg.test(c) ||
+        elipses_reg.test(c)
+      )
+    ) {
       chunks[i + 1] = c + (chunks[i + 1] || '');
-    } else if (c && c.length > 0 && /[a-zA-Z]/.test(c)) {
+    } else if (c && c.length > 0 && /[a-z]/i.test(c)) {
       //this chunk is a proper sentence..
       sentences.push(c);
       chunks[i] = '';

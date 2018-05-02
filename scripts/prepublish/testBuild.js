a38b21465fc5a2719ccd6dc3d677d80830004b71
spencermountain <spencermountain@gmail.com>
2018-05-02 17:12:34 -0400

    update test reporter

--- scripts/prepublish/testBuild.js
+++ scripts/prepublish/testBuild.js
@@ -1,6 +1,6 @@
 var exec = require('shelljs').exec;
 var tape = '"node_modules/.bin/tape"';
-var tapSpec = '"node_modules/.bin/tap-spec" --color';
+var tapSpec = '"node_modules/.bin/tap-dancer" --color';
 
 console.log('testing  built package..');
 exec('TESTENV=prod ' + tape + ' "./test/unit/**/*.test.js" | ' + tapSpec);

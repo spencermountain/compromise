ff48383e5a4c98579999c68d627aa6c45f13fefe
spencer kelly <spencermountain@gmail.com>
2018-09-01 15:49:00 -0400

    #515

--- scratch.js
+++ scratch.js
@@ -1,12 +1,9 @@
-var nlp = require('./src/index');
+var nlp = require("./src/index");
 // nlp.verbose('tagger');
 
 // var doc = nlp('in north africa, eastern asia, guatemala, europe, north america, and japan');
 // doc.places().debug();
 
-
-let text = 'and "Dig Your own grave and Save".';
+let text = "hello (No. 08462148) world";
 let doc = nlp(text);
 doc.debug();
-// let matches = doc.quotations().out('array');
-// console.log(matches);

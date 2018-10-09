10a3bb292b99a592a08501793288b6922ca0a17f
spencer kelly <spencermountain@gmail.com>
2018-10-09 14:03:51 -0400

    fixes for #520

--- scratch.js
+++ scratch.js
@@ -1,10 +1,7 @@
 var nlp = require('./src/index');
-// nlp.verbose('tagger');
+nlp.verbose('tagger');
 
 // var doc = nlp('in north africa, eastern asia, guatemala, europe, north america, and japan');
 // doc.places().debug();
 
-let doc = nlp('foo bar');
-let rel = doc.match('asdf');
-doc.delete(rel);
-//:boom:
+console.log(nlp('attempted').verbs().toInfinitive().out());

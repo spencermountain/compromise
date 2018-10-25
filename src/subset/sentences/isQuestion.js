0422d9826a3686dad0110aba60736fb4b70c760c
Shamoon Siddiqui <shamoon@develop.io>
2018-10-25 14:33:05 -0400

    Trying to get the dots at the end to work

--- src/subset/sentences/isQuestion.js
+++ src/subset/sentences/isQuestion.js
@@ -9,32 +9,45 @@ const isQuestion = function (ts) {
     // If it has a question mark
     // e.g., Is this the real life?
     case /\?/.test(endPunct) === true:
+      console.log('1')
       return true;
 
     // Has ellipsis at the end means it's probably not a question
     // e.g., Is this just fantasy...
     case ts.has('\\.\\.$'):
+      console.log('2')
       return false;
 
     // Starts with question word, but has a comma, so probably not a question
     // e.g., Why are we caught in a land slide, no escape from reality
     case ts.has('^#QuestionWord') && ts.has('#Comma'):
+      console.log('3')
       return false;
 
     // Starts with a #QuestionWord
-    // e.g., How open your eyes
+    // e.g., What open your eyes look up to the skies and see
     case ts.has('^#QuestionWord'):
+      console.log('4')
       return true;
 
-    //is it, do you - start of sentence
-    // e.g., Will you escape from reality
-    case ts.has('^(do|does|did|is|was|can|could|will|would|may) #Noun'):
-      return true;
-
-    // Second word is a question word
-    // e.g., Open what eyes
+    // Second word is a #QuestionWord
+    // e.g., I'm what a poor boy
     case ts.has('^\w+\s#QuestionWord'):
+      console.log('5')
       return true;
+
+    // is it, do you - start of sentence
+    // e.g., Do I need no sympathy
+    case ts.has('^(do|does|did|is|was|can|could|will|would|may) #Noun'):
+      console.log('6')
+      return true;
+
+    // these are a little more loose..
+    // e.g., Must I  be come easy come easy go
+    case ts.has('^(have|must) you'):
+      console.log('7')
+      return true;
+
 
     // Probably not a question
     default:

08c969dd0062816e6bd6346c86388ae0151abe70
spencer kelly <spencermountain@gmail.com>
2018-12-03 23:49:15 -0500

    better test coverage reports

--- scripts/postpublish/coverage.js
+++ scripts/postpublish/coverage.js
@@ -1,12 +1,7 @@
 var exec = require('shelljs').exec;
+var codecov = '15039ad1-b495-48cd-b4a0-bcf124c9b318'; //i don't really care if you steal this.
 
-//to upload to codacity, set the api key as $CODACY_PROJECT_TOKEN
-var obf = process;
-obf.env['CODECOV_TO' + 'KEN'] = '15039ad1-b495-48cd-b4a0-bcf124c9b318'; //i don't really care if you steal this.
 //run all the tests
-console.log('\n 🏃  running coverage tests..');
-
-// let cmd = `./node_modules/.bin/istanbul cover ./tests/*.test.js --report lcovonly -- -R spec && cat ./coverage/lcov.info | codacy-coverage && rm -rf ./coverage`;
-let cmd = `./node_modules/.bin/nyc report --reporter=text-lcov > coverage.lcov && codecov`;
+let cmd = `./node_modules/.bin/nyc --reporter=text-lcov ./node_modules/.bin/tape ./test/unit/**/*.test.js > coverage.lcov && ./node_modules/.bin/codecov -t ${codecov}`;
 exec(cmd);
 console.log('\n 🏃 done!');

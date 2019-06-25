c9bc6c2d4779b1d51cc875b9ee5dd72a29091c50
Spencer Kelly <spencermountain@gmail.com>
2019-06-25 13:07:20 -0400

    dont change eslint yet

--- scripts/build/es6-build.js
+++ scripts/build/es6-build.js
@@ -1,25 +1,25 @@
-const shelljs = require('shelljs')
-const chalk = require('chalk')
-const fileSize = require('../lib/filesize')
-const pkg = require('../../package.json')
-const exec = shelljs.exec
-const echo = shelljs.echo
+var shelljs = require('shelljs')
+var chalk = require('chalk')
+var fileSize = require('../lib/filesize')
+var pkg = require('../../package.json')
+var exec = shelljs.exec
+var echo = shelljs.echo
 
-const browserify = '"node_modules/.bin/browserify"'
-const derequire = '"node_modules/.bin/derequire"'
-const babili = '"node_modules/.bin/babili"'
+var browserify = '"node_modules/.bin/browserify"'
+var derequire = '"node_modules/.bin/derequire"'
+var babili = '"node_modules/.bin/babili"'
 
-const es6 = './builds/compromise.es6.js'
-const es6min = './builds/compromise.es6.min.js'
+var es6 = './builds/compromise.es6.js'
+var es6min = './builds/compromise.es6.min.js'
 
-const banner =
+var banner =
   '/* compromise v' + pkg.version + '\n   http://compromise.cool\n   MIT\n*/\n'
 echo(banner).to(es6)
 
 console.log(chalk.yellow(' 🕑 creating es6 build..'))
 
 //es6 main (browserify)
-let cmd = browserify + ' "./src/index.js" --standalone nlp'
+var cmd = browserify + ' "./src/index.js" --standalone nlp'
 // cmd += ' -p bundle-collapser/plugin';
 cmd += ' | ' + derequire
 cmd += ' >> ' + es6

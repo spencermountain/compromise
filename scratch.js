56e355a29ae6fd787a6388535b7e237aaa588da6
spencer kelly <spencermountain@gmail.com>
2019-07-16 13:34:20 -0400

    contraction gets implicit text

--- scratch.js
+++ scratch.js
@@ -6,6 +6,8 @@ var nlp = require('./src/index')
 // match.insertAt(match, str)
 // phrase.insertAt(index, str)
 var m = nlp(`she wouldn't go to the store`).debug()
+console.log(m.text())
+console.log(m.normal())
 
 // nlp(`she would have like to go`)
 //   .insertAfter(`would`, 'not')
--- src/tagger/03-contractions/index.js
+++ src/tagger/03-contractions/index.js
@@ -3,6 +3,24 @@ const checkApostrophe = require('./02-apostrophe-s')
 const checkIrregulars = require('./03-irregulars')
 const build = require('../../tokenizer')
 
+const createPhrase = function(found, doc) {
+  //create phrase from ['would', 'not']
+  let phrase = build.fromText(found.join(' '), doc.pool())[0]
+  //tag it
+  let tmpDoc = doc.buildFrom([phrase])
+  tmpDoc.tagger()
+  //make these terms implicit
+  phrase.terms().forEach((t, i) => {
+    t.implicit = t.text
+    t.text = ''
+    // remove whitespace for implicit terms
+    if (i > 0) {
+      t.preText = ''
+    }
+  })
+  return phrase
+}
+
 const contractions = function(doc) {
   doc.list.forEach(p => {
     let terms = p.terms()
@@ -13,12 +31,9 @@ const contractions = function(doc) {
       found = found || checkIrregulars(term)
       //add them in
       if (found !== null) {
-        //create phrase from ['would', 'not']
-        let newPhrase = build.fromText(found.join(' '), doc.pool())[0]
-        //tag it
-        let tmpDoc = doc.buildFrom([newPhrase])
-        tmpDoc.tagger()
-        // newPhrase.tagger()
+        let newPhrase = createPhrase(found, doc)
+        //set text as contraction
+        newPhrase.terms(0).text = term.text
         //grab sub-phrase to remove
         let match = p.buildFrom(term.id, 1, doc.pool())
         match.replace(newPhrase, doc)

ebdebdda384e4b983ebca1d8b12e351c710c7845
spencer kelly <spencermountain@gmail.com>
2019-09-23 15:38:16 -0400

    31 failing tests

--- scratch.js
+++ scratch.js
@@ -3,9 +3,10 @@ var nlp = require('./src/index')
 // nlp.extend(require('./plugins/verbs/src'))
 // nlp.extend(require('./plugins/ngrams/src'))
 
-var lexicon = {
-  'bed bath and beyond': 'Organization',
-}
-nlp('shopping at Bed Bath and Beyond the store', lexicon).debug()
+// var lexicon = {
+//   'bed bath and beyond': 'Organization',
+// }
+// nlp('shopping at Bed Bath and Beyond the store', lexicon).debug()
 // var str = r.organizations().out('normal')
 // console.log(str)
+nlp('jean jacket. jean Slkje').debug()
---
+++ tests/tagger/organization.test.js
@@ -0,0 +1,25 @@
+var test = require('tape')
+var nlp = require('../_lib')
+
+test('organization test', function(t) {
+  var arr = [
+    'google',
+    'google inc',
+    'Capital One',
+    'HSBC',
+    'NASA',
+    '7-eleven',
+    'al qaeda',
+    'FBI',
+    'monsanto',
+    'Johnson & Johnson',
+    // 'Johnson & Johnson LLC',
+  ]
+  arr.forEach(function(str) {
+    var r = nlp(str)
+    var orgs = r.organizations()
+    var msg = orgs.out('text') + '  -  ' + str
+    t.equal(orgs.out('text'), str, msg)
+  })
+  t.end()
+})

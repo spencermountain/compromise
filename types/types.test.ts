b93386707cc821e6fdbc0cd6ed03ffe6debfa5d2
Dominic Cassidy <dcassidy@arago.de>
2020-01-07 12:53:08 +0000

    Add full typings for number plugin to test

--- types/types.test.ts
+++ types/types.test.ts
@@ -4,13 +4,31 @@ import nlp from '../'
 import nlpNumbers from '../plugins/numbers'
 
 // Typings for imported plugin
+interface NumberFunctions {
+  json(n?: number): Document
+  fractions(): Document
+  toText(): Document
+  toNumber(): Document
+  toOrdinal(): Document
+  toCardinal(): Document
+  add(n: number): Document
+  subtract(n: number): Document
+  increment(): Document
+  decrement(): Document
+  isEqual(): Document
+  greaterThan(min: number): Document
+  lessThan(max: number): Document
+  between(min: number, max: number): Document
+  isOrdinal(): Document
+  isCardinal(): Document
+  toLocaleString(): Document
+}
+
 type NLPNumbers = nlp.Plugin<
   {
-    numbers: () => number[]
+    numbers(n?: number): NumberFunctions
   },
-  {
-    a: string
-  }
+  {}
 >
 
 // vs Typed plugin
@@ -30,7 +48,7 @@ const nlpEx = nlp
 const doc = nlpEx('hello world')
 doc.test('test')
 doc.numbers()
-doc.world.a === typeof 'string'
+doc.numbers().json()
 doc.world.test === typeof 'string'
 
 // Demo: For external use

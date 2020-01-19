911a86f25e5032eca1cd4ad62e4709c2b5d9eb0e
Dominic Cassidy <dcassidy@arago.de>
2020-01-19 10:54:27 +0000

    Move issue tests to issue folder

---
+++ tests/issues/issue-654.test.js
@@ -0,0 +1,51 @@
+const test = require('tape')
+const nlp = require('../_lib')
+
+/*
+ * Capture group doesn't work for .+ or *
+ * https://github.com/spencermountain/compromise/issues/654
+ */
+
+test('issue-654: named greedy capture', function(t) {
+  let m
+
+  m = nlp('ralf eats the glue')
+    .match('ralf eats [<target>*]')
+    .named()
+  t.equal(m.out('normal'), 'the glue', 'wildcard capture at the end')
+
+  m = nlp('ralf eats the glue')
+    .match('ralf eats [<target>*] glue')
+    .named()
+  t.equal(m.out('normal'), 'the', 'wildcard capture in the middle')
+
+  m = nlp('ralf eats the glue')
+    .match('ralf eats [<target>.+]')
+    .named()
+  t.equal(m.out('normal'), 'the glue', 'wildcard capture at the end')
+
+  m = nlp('ralf eats the glue')
+    .match('ralf eats [<target>.+] glue')
+    .named()
+  t.equal(m.out('normal'), 'the', 'wildcard capture in the middle')
+
+  t.end()
+})
+
+test('issue-654: greedy capture', function(t) {
+  let m
+
+  m = nlp('ralf eats the glue').match('ralf eats [*]')
+  t.equal(m.out('normal'), 'the glue', 'wildcard capture at the end')
+
+  m = nlp('ralf eats the glue').match('ralf eats [*] glue')
+  t.equal(m.out('normal'), 'the', 'wildcard capture in the middle')
+
+  m = nlp('ralf eats the glue').match('ralf eats [.+]')
+  t.equal(m.out('normal'), 'the glue', 'wildcard capture at the end')
+
+  m = nlp('ralf eats the glue').match('ralf eats [.+] glue')
+  t.equal(m.out('normal'), 'the', 'wildcard capture in the middle')
+
+  t.end()
+})

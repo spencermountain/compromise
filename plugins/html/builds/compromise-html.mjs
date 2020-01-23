40d25caed78bd805caf2233ea2edd878cee45f4f
spencer kelly <spencermountain@gmail.com>
2020-01-23 10:33:32 -0500

    finish updating deps

--- plugins/html/builds/compromise-html.mjs
+++ plugins/html/builds/compromise-html.mjs
@@ -10,45 +10,35 @@ function _taggedTemplateLiteral(strings, raw) {
   }));
 }
 
-var n = function n(t, r, u, e) {
-  for (var p = 1; p < r.length; p++) {
-    var s = r[p],
-        h = "number" == typeof s ? u[s] : s,
-        a = r[++p];
-    1 === a ? e[0] = h : 3 === a ? e[1] = Object.assign(e[1] || {}, h) : 5 === a ? (e[1] = e[1] || {})[r[++p]] = h : 6 === a ? e[1][r[++p]] += h + "" : e.push(a ? t.apply(null, n(t, h, u, ["", null])) : h);
+var n = function n(t, s, r, e) {
+  var u;
+  s[0] = 0;
+
+  for (var h = 1; h < s.length; h++) {
+    var p = s[h++],
+        a = s[h] ? (s[0] |= p ? 1 : 2, r[s[h++]]) : s[++h];
+    3 === p ? e[0] = a : 4 === p ? e[1] = Object.assign(e[1] || {}, a) : 5 === p ? (e[1] = e[1] || {})[s[++h]] = a : 6 === p ? e[1][s[++h]] += a + "" : p ? (u = t.apply(a, n(t, a, r, ["", null])), e.push(u), a[0] ? s[0] |= 2 : (s[h - 2] = 0, s[h] = u)) : e.push(a);
   }
 
   return e;
 },
-    t = function t(n) {
-  for (var t, r, u = 1, e = "", p = "", s = [0], h = function h(n) {
-    1 === u && (n || (e = e.replace(/^\s*\n\s*|\s*\n\s*$/g, ""))) ? s.push(n || e, 0) : 3 === u && (n || e) ? (s.push(n || e, 1), u = 2) : 2 === u && "..." === e && n ? s.push(n, 3) : 2 === u && e && !n ? s.push(!0, 5, e) : u >= 5 && ((e || !n && 5 === u) && (s.push(e, u, r), u = 6), n && (s.push(n, u, r), u = 6)), e = "";
-  }, a = 0; a < n.length; a++) {
-    a && (1 === u && h(), h(a));
+    t = new Map();
 
-    for (var f = 0; f < n[a].length; f++) {
-      t = n[a][f], 1 === u ? "<" === t ? (h(), s = [s], u = 3) : e += t : 4 === u ? "--" === e && ">" === t ? (u = 1, e = "") : e = t + e[0] : p ? t === p ? p = "" : e += t : '"' === t || "'" === t ? p = t : ">" === t ? (h(), u = 1) : u && ("=" === t ? (u = 5, r = e, e = "") : "/" === t && (u < 5 || ">" === n[a][f + 1]) ? (h(), 3 === u && (s = s[0]), u = s, (s = s[0]).push(u, 2), u = 0) : " " === t || "\t" === t || "\n" === t || "\r" === t ? (h(), u = 2) : e += t), 3 === u && "!--" === e && (u = 4, s = s[0]);
+function htm (s) {
+  var r = t.get(this);
+  return r || (r = new Map(), t.set(this, r)), (r = n(this, r.get(s) || (r.set(s, r = function (n) {
+    for (var t, s, r = 1, e = "", u = "", h = [0], p = function p(n) {
+      1 === r && (n || (e = e.replace(/^\s*\n\s*|\s*\n\s*$/g, ""))) ? h.push(0, n, e) : 3 === r && (n || e) ? (h.push(3, n, e), r = 2) : 2 === r && "..." === e && n ? h.push(4, n, 0) : 2 === r && e && !n ? h.push(5, 0, !0, e) : r >= 5 && ((e || !n && 5 === r) && (h.push(r, 0, e, s), r = 6), n && (h.push(r, n, 0, s), r = 6)), e = "";
+    }, a = 0; a < n.length; a++) {
+      a && (1 === r && p(), p(a));
+
+      for (var l = 0; l < n[a].length; l++) {
+        t = n[a][l], 1 === r ? "<" === t ? (p(), h = [h], r = 3) : e += t : 4 === r ? "--" === e && ">" === t ? (r = 1, e = "") : e = t + e[0] : u ? t === u ? u = "" : e += t : '"' === t || "'" === t ? u = t : ">" === t ? (p(), r = 1) : r && ("=" === t ? (r = 5, s = e, e = "") : "/" === t && (r < 5 || ">" === n[a][l + 1]) ? (p(), 3 === r && (h = h[0]), r = h, (h = h[0]).push(2, 0, r), r = 0) : " " === t || "\t" === t || "\n" === t || "\r" === t ? (p(), r = 2) : e += t), 3 === r && "!--" === e && (r = 4, h = h[0]);
+      }
     }
-  }
 
-  return h(), s;
-},
-    r = "function" == typeof Map,
-    u = r ? new Map() : {},
-    e = r ? function (n) {
-  var r = u.get(n);
-  return r || u.set(n, r = t(n)), r;
-} : function (n) {
-  for (var r = "", e = 0; e < n.length; e++) {
-    r += n[e].length + "-" + n[e];
-  }
-
-  return u[r] || (u[r] = t(n));
-};
-
-function htm (t) {
-  var r = n(this, e(t), arguments, []);
-  return r.length > 1 ? r : r[0];
+    return p(), h;
+  }(s)), r), arguments, [])).length > 1 ? r : r[0];
 }
 
 var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};

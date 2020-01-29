54dc57f2e8b14b2442bc09d529fd91f3381ca968
Dominic Cassidy <dcassidy@arago.de>
2020-01-29 22:25:48 +0000

    Add test

--- tests/match/named-match.test.js
+++ tests/match/named-match.test.js
@@ -169,6 +169,23 @@ test('named-match-overlap', function(t) {
   t.end()
 })
 
+test('named-object-match-quick:', function(t) {
+  let arr = [
+    ['the dog played', [{ word: 'the' }, { tag: 'Noun', named: 'target' }, { word: 'played' }], 'dog'],
+    ['the dog played', [{ word: 'dog', named: 'target' }], 'dog'],
+    ['the dog played', [{ tag: 'Verb', named: 'target' }], 'played'],
+  ]
+
+  arr.forEach(function(a) {
+    const doc = nlp(a[0]).match(a[1], 'target')
+
+    const msg = a[0] + ' matches ' + JSON.stringify(a[1]) + ' ' + a[2]
+    t.equal(doc.text(), a[2], msg)
+  })
+
+  t.end()
+})
+
 test('named-object-match:', function(t) {
   let arr = [
     ['the dog played', [{ word: 'the' }, { tag: 'Noun', named: 'target' }, { word: 'played' }], 'dog'],

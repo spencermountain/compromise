127f0b7fe15edca0c6706c7ced66d8ce0a63097e
spencer kelly <spencermountain@gmail.com>
2020-02-05 22:09:22 -0500

    tests still passing, play with cache

--- scratch.js
+++ scratch.js
@@ -5,4 +5,4 @@ const nlp = require('./src/index')
 
 // nlp(`okay, do not use reverse psychology`).debug()
 // nlp(`April, June, and September`).debug()
-nlp(`really mark`).debug()
+nlp(`really mark. tony is cool. r u okay?`).debug()
--- src/02-tagger/04-correction/runner.js
+++ src/02-tagger/04-correction/runner.js
@@ -1,12 +1,57 @@
-const list = require('./_corrections')
+let matches = require('./_corrections')
+const parseSyntax = require('../../Doc/match/syntax')
+
+// matches = matches.slice(0, 3)
+matches = matches.map(a => {
+  let needTags = []
+  let needWords = []
+  let reg = parseSyntax(a[0])
+  reg.forEach(obj => {
+    if (obj.optional === true) {
+      return
+    }
+    if (obj.tag !== undefined) {
+      needTags.push(obj.tag)
+    }
+    if (obj.word !== undefined) {
+      needWords.push(obj.word)
+    }
+  })
+  return {
+    reg: reg,
+    needTags: needTags,
+    needWords: needWords,
+    group: a[1],
+    tag: a[2],
+    hint: a[3],
+    tagSafe: a[4],
+    str: a[0],
+  }
+})
+console.log('all:', matches.length)
+console.log('words:', matches.filter(m => m.needWords.length > 0).length)
+console.log('tags:', matches.filter(m => m.needTags.length > 0).length)
+console.log('either:', matches.filter(m => m.needWords.length > 0 || m.needTags.length > 0).length)
 
 const runner = function(doc) {
-  list.forEach(c => {
+  //find phrases to try for each match
+  matches.forEach(m => {
+    // let fromTags = []
+    // let fromWords = []
+    // m.needWords.forEach(w => {
+    //   if (doc._cache.words[m.needWords] !== undefined) {
+    //     fromWords = fromWords.concat(doc._cache.words[m.needWords])
+    //   }
+    // })
+    // if (fromWords.length) {
+    //   console.log(fromWords)
+    //   console.log(m.str)
+    // }
     // tagsafe
-    if (c[4] === true) {
-      doc.match(c[0], c[1]).tagSafe(c[2], c[3])
+    if (m.tagSafe === true) {
+      doc.match(m.reg, m.group).tagSafe(m.tag, m.hint)
     } else {
-      doc.match(c[0], c[1]).tag(c[2], c[3])
+      doc.match(m.reg, m.group).tag(m.tag, m.hint)
     }
   })
 }

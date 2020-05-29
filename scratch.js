cfb04b89ad504893f3a92ffd7b215945003ae620
spencer kelly <spencermountain@gmail.com>
2020-05-28 20:22:05 -0400

    add viktor for #652

--- scratch.js
+++ scratch.js
@@ -1,10 +1,7 @@
 const nlp = require('./src/index')
-// nlp.verbose(true)
+nlp.verbose(true)
 // let txt = require('./scripts/test/speed/_sotu-text.js')
-nlp.extend(require('./plugins/sentences/src'))
+// nlp.extend(require('./plugins/sentences/src'))
 
-const txt = `Probably the renovation, which has been done to the property.`
-let doc = nlp(txt)
-// let doc = nlp('is wayne gretskzy alive')
-doc.clauses().debug()
-// doc.sentences().isQuestion().debug()
+let res = nlp('am being driven').verbs().debug().conjugate()[0]
+console.log(res)

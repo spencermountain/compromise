b3eeac7626aac78eb1a10e5fa3e1c2ea38e7f034
spencer kelly <spencermountain@gmail.com>
2020-06-10 14:43:23 -0400

    punt #737 do another release

--- scratch.js
+++ scratch.js
@@ -1,24 +1,18 @@
 const nlp = require('./src/index')
 // nlp.verbose(true)
 // let txt = require('./scripts/test/speed/_sotu-text.js')
-nlp.extend(require('./plugins/numbers/src'))
-nlp.extend(require('./plugins/dates/src'))
+// nlp.extend(require('./plugins/numbers/src'))
+// nlp.extend(require('./plugins/dates/src'))
+nlp.extend(require('./plugins/sentences/src'))
 
 // // let doc = nlp('i am being driven')
 // let doc = nlp('i should be driven')
-// // let doc = nlp('i should have been driven')
+// let doc = nlp('i should drive')
+let doc = nlp('i should drive')
+// let doc = nlp('i should have been driven')
 // doc.sentences().toFutureTense().debug()
-
-// let doc = nlp('open').debug()
+doc.sentences().toPastTense().debug()
 
 // console.log(nlp('i was born on august 11th 1998').dates().format('{date}-{month}-{year}').text())
 // console.log(nlp('i was born in august 11th 1998').dates().format('{date}-{month}-{year}').text())
 // console.log(nlp('i was born august 11th 1998').dates().format('{date}-{month}-{year}').text())
-
-// let doc = nlp('i was born on august 11th 1998')
-// doc.debug()
-// doc.dates().debug()
-nlp(`what companies are doing is`).debug()
-
-const arr = [['some', 'array', 'of text and such']]
-nlp(arr).debug()

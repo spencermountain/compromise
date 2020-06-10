f912dc8e4deaad22e367d3775cdb28db8ceecb2a
spencer kelly <spencermountain@gmail.com>
2020-06-10 12:34:43 -0400

    phone number improvements #761

--- scratch.js
+++ scratch.js
@@ -1,19 +1,12 @@
 const nlp = require('./src/index')
-// nlp.verbose(true)
+nlp.verbose(true)
 // let txt = require('./scripts/test/speed/_sotu-text.js')
 nlp.extend(require('./plugins/sentences/src'))
 
-// let doc = nlp('i am being driven')
-// doc.debug()
-// let doc = nlp('he has been stalking his prey')
-// 'Our pet alligator'
-let doc = nlp('he should have been eating Gator Chow')
+// // let doc = nlp('i am being driven')
+// let doc = nlp('i should be driven')
+// // let doc = nlp('i should have been driven')
+// doc.sentences().toFutureTense().debug()
 
-// let doc = nlp('Joelle bit her tongue instead of criticizing her prom date.')
-// doc.sentences().toPastTense().debug()
-// doc.sentences().toPresentTense().debug()
-doc.sentences().toFutureTense().debug()
-
-doc = nlp('I am being driven to madness.')
-let res = doc.verbs().debug().conjugate()[0]
-console.log(res)
+let doc = nlp('+1-123-444-55555')
+doc.debug()
--- tests/verbs/misc.test.js
+++ tests/verbs/misc.test.js
@@ -1,10 +1,8 @@
 const test = require('tape')
 const nlp = require('../_lib')
 
-test('verbs.json', function(t) {
-  let json = nlp('She has called twice, not the tv')
-    .verbs()
-    .json()
+test('verbs.json', function (t) {
+  let json = nlp('She has called twice, not the tv').verbs().json()
   t.equal(json.length, 1, 'one verb')
   t.equal(json[0].isNegative, false, 'not negative')
   t.equal(json[0].parts.verb, 'called', 'got main verb')
@@ -12,17 +10,12 @@ test('verbs.json', function(t) {
   t.end()
 })
 
-test('verbs.adverbs', function(t) {
+test('verbs.adverbs', function (t) {
   let doc = nlp('spencer is really great! Spencer really really was superb.')
-  doc
-    .verbs()
-    .adverbs()
-    .delete()
+  doc.verbs().adverbs().delete()
   t.equal(doc.out(), 'spencer is great! Spencer was superb.', 'no-adverbs')
 
-  doc = nlp('spencer truly would really run quickly')
-    .verbs()
-    .adverbs()
+  doc = nlp('spencer truly would really run quickly').verbs().adverbs()
   t.equal(doc.length, 3, 'found all three adverbs')
   t.equal(doc.text('reduced'), 'truly really quickly', 'found adverbs in order')
 

28596d584863b36a9270d4c848d74ec38e3c2974
spencer kelly <spencermountain@gmail.com>
2020-09-14 15:15:47 -0400

    finally reproduce offset bug in compromise

--- scratch.js
+++ scratch.js
@@ -1,22 +1,11 @@
 const nlp = require('./src/index')
-const spacetime = require('/Users/spencer/mountain/spacetime/src')
+// const spacetime = require('/Users/spencer/mountain/spacetime/src')
 // nlp.verbose(true)
 // let txt = require('./scripts/test/speed/_sotu-text.js')
-nlp.extend(require('./plugins/numbers/src'))
-nlp.extend(require('./plugins/dates/src'))
+// nlp.extend(require('./plugins/numbers/src'))
+// nlp.extend(require('./plugins/dates/src'))
 // nlp.extend(require('./plugins/sentences/src'))
 
-// let doc = nlp('april fools').debug()
-// console.log(spacetime(dates.date.start).format('{day-short} {month-short} {date} {year}, {time}'))
-
-// let doc = nlp(`once a month`)
-let doc = nlp(`wednesday`)
-
-// doc.debug()
-let dates = doc.dates().json(0)
-console.log(dates)
-if (dates.date.end) {
-  console.log(spacetime(dates.date.end).format('{day-short} {month-short} {date} {year}, {time}'))
-} else {
-  console.log('--')
-}
+let doc = nlp(`so I have not arranged`)
+doc = doc.clone()
+console.log(doc.json({ offset: true, normal: true })[0].offset)

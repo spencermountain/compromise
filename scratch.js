2acbec05ff691699ae3063cf3857b23e308ef962
spencer kelly <spencermountain@gmail.com>
2020-10-03 11:13:01 -0400

    add code-coverage github action

--- scratch.js
+++ scratch.js
@@ -7,12 +7,17 @@ nlp.extend(require('./plugins/dates/src'))
 // nlp.extend(require('./plugins/sentences/src'))
 
 // let doc = nlp(`before 2017`)
-let doc = nlp(`a month from now`)
-// let doc = nlp(`last week`)
-let february = 1
-let today= [2016, february, 5] // a friday
-// let doc = nlp(`sunday`)
+// // let doc = nlp(`last week`)
+// let february = 1
+// let today = [2016, february, 5] // a friday
+// // let doc = nlp(`sunday`)
 // doc.debug()
-// let doc = nlp(`a year ago`)
-let obj = doc.dates({today:today}).json()[0]
-console.log(spacetime(obj.date.start).format('nice-day'))
+// // let doc = nlp(`a year ago`)
+// let obj = doc.dates({ today: today }).json()[0]
+// console.log(spacetime(obj.date.start).format('{nice-day} {year}'))
+
+let doc = nlp(`
+What to stream this weekend: Birds of Prey, The Walking Dead: Word Beyond and more highlights`)
+const knownTerms = ['Birds of Prey', 'The Walking Dead', 'Word Beyond']
+
+doc.lookup(knownTerms).debug()

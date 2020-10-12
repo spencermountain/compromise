76b0e5944543c4cdadca956df8d57a103cb203eb
spencer kelly <spencermountain@gmail.com>
2020-10-12 13:28:31 -0400

    more tests for subject method

--- tests/verbs/subject.test.js
+++ tests/verbs/subject.test.js
@@ -17,6 +17,28 @@ test('verb.subject():', function (t) {
     ['if so, john should pay for it', 'john'],
     ['you really think so?', 'you'],
     ['the moon is made of cheese', 'moon'],
+    [`listen to the wind blow, watch the sun rise.`, ''],
+    [`run in the shadows, damn your love, damn your lies. `, ''],
+    [`listen to the wind blow down comes the night. `, ''],
+    [`running in the shadows damn your love, damn your lies. `, ''],
+    [`break the silence, damn the dark, damn the light.`, ''],
+    [`some should decrease.`, ''],
+    [`join the community of conscience.`, ''],
+    [`why is everything different?`, ''],
+    [`bye!`, ''],
+    [`oh dear god!`, ''],
+    [`is there something wrong?`, ''],
+    [`why is ross naked?`, ''],
+    [`yeah the big homey`, ''],
+    [`the type of female wit fly gucci gear`, ''],
+    [`compared to the heart of a man`, ''],
+    [`give me a red rose, she cried`, ''],
+    [`drinking orange juice out of a champagne glass.`, ''],
+    //
+    // [`it moves power and decision making closer to the people.`, 'it'],
+    // [`that is completely untrue.`, 'that'],
+    // [`i better hit the shower.`, 'i'],
+    // [`morning's here!`, 'morning'],
   ]
   arr.forEach(function (a) {
     const str = nlp(a[0]).verbs(0).subject().text()
@@ -24,3 +46,14 @@ test('verb.subject():', function (t) {
   })
   t.end()
 })
+
+// let clauses = [
+//   `once established, federal programs seem to become immortal.`,
+//   `working with congress and the governors, i propose we select at least $15 billion`,
+//   `that's  why i'm  submitting a budget `,
+//   `what is at stake is more than one small country, it is a big idea`,
+//   `i mean its amazing pheebs.`,
+//   `believe me, i lived with her for 16 years.`,
+//   `in 2003 alone, according to state government data, there were over 6.4 million visitors`,
+//   `but wait i hear they're  prissy, bourgeois, all that`,
+// ]

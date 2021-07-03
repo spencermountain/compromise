cbf81e62f8832fc3d4cd6ab14d7798235b877e59
spencer kelly <spencerkelly86@gmail.com>
2021-07-03 16:44:30 -0400

    at-methods work in match again

--- scratch.js
+++ scratch.js
@@ -6,10 +6,11 @@ import nlp, { model } from './src/index.js'
 nlp.verbose(true)
 // let doc = nlp(`Howard Stern`)
 // let doc = nlp(`Cameron Diaz`)
-let doc = nlp(`let her win`)
-doc.debug()
+let doc = nlp(`Cameron, too`)
+doc.match('@hasComma').debug()
+// doc.debug()
 
-// console.log(nlp.parseMatch('(Value && one)'))
+// console.log(nlp.parseMatch('@hasComma'))
 
 // console.log(doc.json())
 // console.log(model.lexicon['healing over'])
--- src/methods/match/03-matchTerm.js
+++ src/methods/match/03-matchTerm.js
@@ -1,8 +1,10 @@
 import fuzzy from './_fuzzy.js'
+import methods from './_match-methods.js'
+
 //declare it up here
 let wrapMatch = function () {}
 /** ignore optional/greedy logic, straight-up term match*/
-const doesMatch = function (t, reg, index, length) {
+const doesMatch = function (term, reg, index, length) {
   // support id matches
   // if (reg.id === t.id) {
   //   return true
@@ -22,65 +24,65 @@ const doesMatch = function (t, reg, index, length) {
   //support a text match
   if (reg.word !== undefined) {
     //match contractions
-    if (t.implicit !== null && t.implicit === reg.word) {
+    if (term.implicit !== null && term.implicit === reg.word) {
       return true
     }
     // term aliases for slashes and things
-    if (t.alias !== undefined && t.alias.hasOwnProperty(reg.word)) {
+    if (term.alias !== undefined && term.alias.hasOwnProperty(reg.word)) {
       return true
     }
     // support ~ match
-    if (reg.soft === true && reg.word === t.root) {
+    if (reg.soft === true && reg.word === term.root) {
       return true
     }
     // support fuzzy match param
     if (reg.fuzzy !== undefined) {
-      let score = fuzzy(reg.word, t.normal)
+      let score = fuzzy(reg.word, term.normal)
       if (score > reg.fuzzy) {
         return true
       }
       // support fuzzy + soft match
       if (reg.soft === true) {
-        score = fuzzy(reg.word, t.root)
+        score = fuzzy(reg.word, term.root)
         if (score > reg.fuzzy) {
           return true
         }
       }
     }
     //match either .clean or .text
-    return reg.word === t.clean || reg.word === t.text || reg.word === t.normal
+    return reg.word === term.clean || reg.word === term.text || reg.word === term.normal
   }
   //support #Tag
   if (reg.tag !== undefined) {
-    return t.tags.has(reg.tag) === true
+    return term.tags.has(reg.tag) === true
   }
   //support @method
   if (reg.method !== undefined) {
-    if (typeof t[reg.method] === 'function' && t[reg.method]() === true) {
+    if (typeof methods[reg.method] === 'function' && methods[reg.method](term) === true) {
       return true
     }
     return false
   }
   //support /reg/
   if (reg.regex !== undefined) {
-    return reg.regex.test(t.clean)
+    return reg.regex.test(term.clean)
   }
   // support optimized (one|two)
   if (reg.fastOr !== undefined) {
-    if (t.implicit && reg.fastOr.has(t.implicit) === true) {
+    if (term.implicit && reg.fastOr.has(term.implicit) === true) {
       return true
     }
-    return reg.fastOr.has(t.normal) || reg.fastOr.has(t.text)
+    return reg.fastOr.has(term.normal) || reg.fastOr.has(term.text)
   }
   //support slower (one|two)
   if (reg.choices !== undefined) {
     // try to support && operator
     if (reg.operator === 'and') {
       // must match them all
-      return reg.choices.every(r => wrapMatch(t, r, index, length))
+      return reg.choices.every(r => wrapMatch(term, r, index, length))
     }
     // or must match one
-    return reg.choices.some(r => wrapMatch(t, r, index, length))
+    return reg.choices.some(r => wrapMatch(term, r, index, length))
   }
   return false
 }

de6602bc91a128e020f78f9c80f057fa0011078b
spencer kelly <spencerkelly86@gmail.com>
2021-08-09 13:15:35 -0400

    update builds

--- rollup.config.js
+++ rollup.config.js
@@ -16,36 +16,36 @@ export default [
 
   // === One ==
   {
-    input: 'src/one/index.js',
+    input: 'src/one/lib.js',
     output: [{ file: 'builds/one/compromise-one.cjs', format: 'umd', name: 'nlp' }],
     plugins: [terser(), sizeCheck({ expect: 40, warn: 15 })],
   },
   {
-    input: 'src/one/index.js',
+    input: 'src/one/lib.js',
     output: [{ file: 'builds/one/compromise-one.mjs', format: 'esm' }],
     plugins: [terser(), sizeCheck({ expect: 40, warn: 15 })],
   },
 
   // === Two ==
   {
-    input: 'src/two/index.js',
+    input: 'src/two/lib.js',
     output: [{ file: 'builds/two/compromise-two.cjs', format: 'umd', name: 'nlp' }],
     plugins: [terser(), sizeCheck({ expect: 160, warn: 15 })],
   },
   {
-    input: 'src/two/index.js',
+    input: 'src/two/lib.js',
     output: [{ file: 'builds/two/compromise-two.mjs', format: 'esm' }],
     plugins: [terser(), sizeCheck({ expect: 160, warn: 15 })],
   },
 
   // === Three ==
   {
-    input: 'src/three/index.js',
+    input: 'src/three/lib.js',
     output: [{ file: 'builds/three/compromise-three.cjs', format: 'umd', name: 'nlp' }],
     plugins: [terser(), sizeCheck({ expect: 160, warn: 15 })],
   },
   {
-    input: 'src/three/index.js',
+    input: 'src/three/lib.js',
     output: [{ file: 'builds/three/compromise-three.mjs', format: 'esm' }],
     plugins: [terser(), sizeCheck({ expect: 160, warn: 15 })],
   },
--- scratch.js
+++ scratch.js
@@ -3,7 +3,7 @@
 import nlp from './src/two/lib.js'
 // nlp.verbose(true)
 
-let doc = nlp(`jack is guarded woops`)
+let doc = nlp(`is guarded woops`)
 let m = doc.match('is guarded foo?$').debug()
 
 /*

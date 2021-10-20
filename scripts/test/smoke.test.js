14c8adcb46ed461d91e3fa87352b2f6327ef76be
spencer kelly <spencerkelly86@gmail.com>
2021-10-20 14:21:49 -0400

    204 tests failing

--- scripts/test/smoke.test.js
+++ scripts/test/smoke.test.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
-const test = require('tape')
-console.log('\n 🎗️  - running smoke-test..\n')
+import test from 'tape'
+import main from '../../builds/compromise.cjs'
+import min from '../../builds/compromise.mjs'
+import tokenize from '../../builds/one/compromise-one.cjs'
+console.log('\n 🎗️  - running smoke-test..\n') // eslint-disable-line
 
 //'sanity-test' the builds
 test('main build', function (t) {
-  const main = require('../../../builds/compromise.js')
   let doc = main('John and Joe walked to the store')
   t.equal(doc.people().json().length, 2, 'found-people')
   t.equal(doc.verbs().json().length, 1, 'found-verbs')
@@ -15,7 +17,6 @@ test('main build', function (t) {
 })
 
 test('min build', function (t) {
-  const min = require('../../../builds/compromise.min.js')
   let doc = min('John and Joe walked to the store')
   t.equal(doc.people().json().length, 2, 'found-people')
   t.equal(doc.verbs().json().length, 1, 'found-verbs')
@@ -26,14 +27,11 @@ test('min build', function (t) {
 })
 
 test('tokenize build', function (t) {
-  const tokenize = require('../../../builds/compromise-tokenize.js')
   let doc = tokenize('John and Joe walked to the store')
-  t.equal(doc.people().json().length, 0, 'found-people')
-  t.equal(doc.verbs().json().length, 0, 'found-verbs')
   t.equal(doc.match('joe walked .').found, true, 'match-statement')
   t.equal(doc.terms(1).text('reduced'), 'and', 'text-out')
   //ensure lexicon works
-  let tmp = tokenize('spencer kelly', { spencer: 'Cool' })
-  t.equal(tmp.match('#Cool').text(), 'spencer', 'lexicon-works')
+  // let tmp = tokenize('spencer kelly', { spencer: 'Cool' })
+  // t.equal(tmp.match('#Cool').text(), 'spencer', 'lexicon-works')
   t.end()
 })

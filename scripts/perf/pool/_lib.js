8e28d5521580fb6fc88417f86cccdfe45d3cfe28
spencer kelly <spencerkelly86@gmail.com>
2021-12-10 16:42:20 -0500

    rc4

--- scripts/perf/pool/_lib.js
+++ scripts/perf/pool/_lib.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 import src from '../../../src/three.js'
+import build from '../../../builds/three/compromise-three.mjs'
 
 let nlp
 if (process.env.TESTENV === 'prod') {
   console.warn('== production build test 🚀 ==')
   // nlp = require('../../../builds/compromise.min.js')
+  nlp = build
 } else {
   nlp = src
   // nlp.extend(require('../plugins/numbers/src'))

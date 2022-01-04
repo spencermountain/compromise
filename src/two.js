9c5c3fc1e5dcef49f6f07f5fa01958b777241512
spencer kelly <spencerkelly86@gmail.com>
2022-01-04 17:33:11 -0500

    done for the day

--- src/two.js
+++ src/two.js
@@ -3,7 +3,7 @@ import preTag from './2-two/preTagger/plugin.js'
 import contractions from './2-two/contraction/plugin.js'
 import postTag from './2-two/postTagger/plugin.js'
 
-nlp.plugin(preTag) //~85kb
+nlp.plugin(preTag) //~103kb  
 nlp.plugin(contractions) //~6kb
 nlp.plugin(postTag) //~33kb
 

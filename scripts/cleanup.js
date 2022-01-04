c39f200b2b727edad9400db75a34b885efd558b9
spencer kelly <spencerkelly86@gmail.com>
2022-01-04 10:42:42 -0500

    remove unused conjugation stuff

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -1,18 +1,7 @@
-import switches from '../lib/lexicon/switches/index.js'
+import models from '../data/models/index.js'
 import lexicon from '../lib/lexicon/index.js'
 
-let all = {}
-let already = {}
-Object.keys(switches).forEach(k => {
-  switches[k].words.forEach(word => {
-    if (lexicon.hasOwnProperty(word)) {
-      all[lexicon[word]] = all[lexicon[word]] || []
-      all[lexicon[word]].push(word)
-    }
-    if (already[word]) {
-      console.log(word)
-    }
-    already[word] = true
-  })
+Object.keys(models).forEach(k => {
+
+
 })
-console.log(all) // eslint-disable-line

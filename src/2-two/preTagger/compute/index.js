515cd00a310a4ac69a7a27e10e63e8abe6eb2220
spencer kelly <spencerkelly86@gmail.com>
2022-03-02 13:27:14 -0500

    move tagrank to one

--- src/2-two/preTagger/compute/index.js
+++ src/2-two/preTagger/compute/index.js
@@ -1,6 +1,5 @@
-import tagRank from './tagRank.js'
 import preTagger from './tagger/index.js'
 import root from './root.js'
 import penn from './penn.js'
 
-export default { preTagger, tagRank, root, penn }
+export default { preTagger, root, penn }

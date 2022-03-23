2ba0f915339eb9582f91560e5ff95c57bdd1ceee
spencer kelly <spencerkelly86@gmail.com>
2022-03-23 11:36:45 -0400

    fix possessive plus pronoun

--- scratch.js
+++ scratch.js
@@ -3,7 +3,7 @@ import nlp from './src/three.js'
 // import plg from './plugins/speech/src/plugin.js'
 // nlp.plugin(plg)
 
-// nlp.verbose('tagger')
+nlp.verbose('tagger')
 // nlp.verbose('chunker')
 
 // weird remove issue
@@ -12,11 +12,5 @@ import nlp from './src/three.js'
 // m.match('three').remove()
 // m.debug()
 
-let doc = nlp('simon says shoot the puck')
-doc.none().normalize().debug()
 
-// let doc = nlp("Wayne's World, party time")
-// let str = doc.people().normalize('heavy').text()
-// console.log(str)
-// "wayne"
-// console.log(doc.text())
+let doc = nlp("Like the time I caught the ferry ").debug()

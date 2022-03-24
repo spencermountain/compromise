758cd5e1e92e1f6c1b670f623dc243847bac371b
spencer kelly <spencerkelly86@gmail.com>
2022-03-24 17:53:05 -0400

    stats demo and export format

--- plugins/stats/rollup.config.js
+++ plugins/stats/rollup.config.js
@@ -1,5 +1,6 @@
 import { terser } from 'rollup-plugin-terser'
 import sizeCheck from 'rollup-plugin-filesize-check'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 const opts = { keep_classnames: true, module: true }
 
@@ -7,16 +8,16 @@ export default [
   {
     input: 'src/plugin.js',
     output: [{ file: 'builds/compromise-stats.cjs', format: 'umd', name: 'compromiseStats' }],
-    plugins: [sizeCheck({ expect: 76, warn: 15 })],
+    plugins: [nodeResolve(), sizeCheck({ expect: 76, warn: 15 })],
   },
   {
     input: 'src/plugin.js',
     output: [{ file: 'builds/compromise-stats.min.js', format: 'umd', name: 'compromiseStats' }],
-    plugins: [terser(opts), sizeCheck({ expect: 67, warn: 15 })],
+    plugins: [nodeResolve(), terser(opts), sizeCheck({ expect: 67, warn: 15 })],
   },
   {
     input: 'src/plugin.js',
     output: [{ file: 'builds/compromise-stats.mjs', format: 'esm' }],
-    plugins: [terser(opts), sizeCheck({ expect: 67, warn: 15 })],
+    plugins: [nodeResolve(), terser(opts), sizeCheck({ expect: 67, warn: 15 })],
   }
 ]

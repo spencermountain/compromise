08308c9494b81e985aab58c7049a7be51bad8d2f
Jamie Femia <badgerr@gmail.com>
2022-05-27 07:49:20 +0100

    make tests a bit more thorough in checking the contents of the clauses

--- tests/three/chunker/clauses.test.js
+++ tests/three/chunker/clauses.test.js
@@ -4,21 +4,23 @@ const here = '[three/clauses] '
 
 test('clauses - with commas', function (t) {
   let arr = [
-    // [string, number of clauses expected]
-    ['fun, cool, etc...', 1],
-    ['cool, and fun', 1],
-    ['just one clause here!', 1],
-    ['if you must, go to the basement', 2],
-    ['an apple, washed and dried', 2],
-    ['one thing, at a time. and then another, right afterwards', 4],
-    ['oh and something else, too', 1],
-    ['tuesday, march 2nd', 1],
-    ['toronto, canada', 1],
-    //['june 6, 1992', 1] // TODO fix '(#Date && @hasComma) #Year' so it gets this one right
+    // ['string', ['expected clauses', 'in an array']]
+    ['fun, cool, etc...', ['fun, cool, etc...']],
+    ['cool, and fun', ['cool, and fun']],
+    ['one, two, and three', ['one, two, and three']],
+    ['just one clause here!', ['just one clause here!']],
+    ['if you must, go to the basement', ['if you must,', 'go to the basement']],
+    ['an apple, washed and dried', ['an apple,', 'washed and dried']],
+    ['one thing, at a time. and then another, right afterwards', ['one thing,', 'at a time.', 'and then another,', 'right afterwards']],
+    ['oh and something else, too', ['oh and something else, too']],
+    ['tuesday, march 2nd', ['tuesday, march 2nd']],
+    ['toronto, canada', ['toronto, canada']],
+    //['june 6, 1992', ['june 6, 1992']] // TODO fix '(#Date && @hasComma) #Year' so it gets this one right
   ]
   arr.forEach(function (a) {
     const clauses = nlp(a[0]).clauses().out('array')
-    t.equal(clauses.length, a[1], here + a[0])
+    t.equal(clauses.length, a[1].length, here + '[number of clauses] ' + a[0])
+    t.deepEqual(clauses, a[1], here + '[clause content] ' + a[0])
   })
   t.end()
 })

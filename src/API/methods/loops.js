7131ca7d2f4c5a5bd270aa7f379b1e089612e5d0
spencer kelly <spencerkelly86@gmail.com>
2022-05-28 16:10:57 -0400

    pass cache down in loops

--- src/API/methods/loops.js
+++ src/API/methods/loops.js
@@ -38,8 +38,10 @@ const map = function (cb, empty) {
 
 const filter = function (cb) {
   let ptrs = this.fullPointer
+  let cache = this._cache || []
   ptrs = ptrs.filter((ptr, i) => {
     let view = this.update([ptr])
+    view._cache = cache[i]
     return cb(view, i)
   })
   return this.update(ptrs)
@@ -47,8 +49,10 @@ const filter = function (cb) {
 
 const find = function (cb) {
   let ptrs = this.fullPointer
+  let cache = this._cache || []
   let found = ptrs.find((ptr, i) => {
     let view = this.update([ptr])
+    view._cache = cache[i]
     return cb(view, i)
   })
   return this.update([found])
@@ -56,8 +60,10 @@ const find = function (cb) {
 
 const some = function (cb) {
   let ptrs = this.fullPointer
+  let cache = this._cache || []
   return ptrs.some((ptr, i) => {
     let view = this.update([ptr])
+    view._cache = cache[i]
     return cb(view, i)
   })
 }

9fd7ac65af5854ea27a26e8dfc6d137a984bf99b
spencer kelly <spencerkelly86@gmail.com>
2022-07-06 11:50:54 -0400

    negative failing tests

--- tests/one/match/negative.test.js
+++ tests/one/match/negative.test.js
@@ -13,7 +13,40 @@ test('! negative match syntax :', function (t) {
 
   doc = nlp.tokenize('one two three')
   m = doc.match('one !foo? two three')
-  t.equal(m.text(), 'one two three', here + 'skip-not')
+  t.equal(m.text(), 'one two three', here + 'optional-not-missing')
+
+
+  doc = nlp("before bar")
+  m = doc.match("before !foo?")
+  t.equal(m.text(), 'before bar', here + 'optional-not-end')
+
+  doc = nlp("before")
+  m = doc.match("before !foo?")
+  t.equal(m.text(), 'before', here + 'optional-not-end-missing')
+
+  m = "i will !not .{0,9}? send"
+  doc = nlp("i will then send him")
+  t.equal(doc.has(m), true, here + 'neg1')
+
+  m = "i will !not? .{0,9}? send"
+  doc = nlp("i will send him")
+  t.equal(doc.has(m), true, here + 'neg-optional')
+
+  m = "i will !not? !not? !not? !not? !not? !not? !not? !not? !not? send"
+  doc = nlp("I will send her.")
+  t.equal(doc.has(m), true, here + 'multi-optional')
+
+  doc = nlp("before bar after")
+  m = doc.match("before !foo? after")
+  t.equal(m.text(), 'before bar after', here + 'optional-not-found')
+
+  m = "i will !not? send"
+  doc = nlp("I will then send him") //this works as a negative but not a positive"
+  t.equal(doc.has(m), true, here + 'neg4')
+
+  m = "i will !not{0,9}? send"
+  doc = nlp("I will then send him") //this does not work with a word between will and send",
+  t.equal(doc.has(m), true, here + 'neg5')
 
   t.end()
 })

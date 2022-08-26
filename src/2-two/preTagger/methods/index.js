63a380fa2410c00a74a8517f8dac2033710d717a
spencer kelly <spencerkelly86@gmail.com>
2022-08-26 12:12:54 -0400

    1x

--- src/2-two/preTagger/methods/index.js
+++ src/2-two/preTagger/methods/index.js
@@ -1,11 +1,14 @@
 import transform from './transform/index.js'
 import expandLexicon from './expand/index.js'
 import quickSplit from './quickSplit.js'
+import looksPlural from './looksPlural.js'
+
 
 export default {
   two: {
     quickSplit,
     expandLexicon,
     transform,
+    looksPlural
   },
 }

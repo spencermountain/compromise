dc00911f98c8ad5d992eecec3cd629ceda396153
spencer kelly <spencerkelly86@gmail.com>
2023-04-18 08:57:34 -0400

    more participles

--- scratch.js
+++ scratch.js
@@ -3,8 +3,30 @@ import nlp from './src/four.js'
 // import plg from './plugins/dates/src/plugin.js'
 // nlp.plugin(plg)
 
-// nlp.verbose('tagger')
+nlp.verbose('tagger')
 
 
-let txt = `1er`
-let doc = nlp(txt).debug()
+/*
+verbs
+"sure",
+"worth",
+"entitled",
+  "better",
+  "ain't",
+  "programme",
+  "gotten",
+"shot",
+  "say.",  
+  "undertaken",
+  "begun",
+    "all",
+  "total",
+"hidden",
+  "firm",
+  */
+
+let arr = [
+  'what companies are doing is'
+]
+let doc = nlp(arr[0]).debug()
+// console.log(doc.json({ root: true })[0])

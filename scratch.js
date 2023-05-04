568b519225dd3933210efb35ae859216f79640b9
spencer kelly <spencerkelly86@gmail.com>
2023-05-04 18:38:08 -0400

    more fixes 12x

--- scratch.js
+++ scratch.js
@@ -3,7 +3,7 @@ import nlp from './src/four.js'
 // import plg from './plugins/dates/src/plugin.js'
 // nlp.plugin(plg)
 
-nlp.verbose('tagger')
+// nlp.verbose('tagger')
 
 let arr = [
 
@@ -61,31 +61,12 @@ let arr = [
 
   // "ink",
 
-  // "unheard",
-  // "unparalleled",
-  // "uninterrupted",
-  // "uninterested",
-  // "untested",
-  // "unnoticed",
-  // "unchallenged",
-  // "unreserved",
-  // "unsettled",
-  // "uninvited",
-  // "unexplained",
-  // "uncircumcised",
-  // "unrequited",
-  // "unmatched",
-  // "unfinished",
+
 
 
   // "overworked",
   // "disinterested",
-  // "untouched",
   // "uprise",
-  // "unopposed",
-  // "undisputed",
-  // "unguarded",
-  // "unrelated",
 
   // "reputed",
   // "overused",
@@ -112,7 +93,10 @@ let arr = [
   // 'manufacturing',
   // 'stream',
   // 'cave',
-  'what companies are doing is',
+  // 'what companies are doing is',
+
+
+
 ]
 let doc = nlp(arr[0]).debug()
 console.log(doc.compute('root').text('root'))
@@ -123,18 +107,3 @@ console.log(doc.compute('root').text('root'))
 // doc.swap('Jeff', 'John')
 // console.log(doc.text())
 // console.log(doc.json()[0])
-
-// import adj from './data/lexicon/adjectives/adjectives.js'
-
-// let adj = ['rich']
-// adj.forEach(str => {
-//   let obj = nlp(str).adjectives().conjugate()[0]
-//   console.log(obj)
-//   if (!obj || !obj.Comparative) {
-//     // console.log(str, obj)
-//     return
-//   }
-//   if (!nlp(obj.Comparative).has('#Comparative')) {
-//     console.log([str, obj.Comparative])
-//   }
-// })

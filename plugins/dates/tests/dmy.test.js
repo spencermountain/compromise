8f88fa9f98bd7e9ef25bf49a4bbc7669577348e0
hchiam <hchiam@users.noreply.github.com>
2024-07-29 23:54:20 -0600

    fix(plugins/dates): add dmy:false to test cases

--- plugins/dates/tests/dmy.test.js
+++ plugins/dates/tests/dmy.test.js
@@ -3,8 +3,11 @@ import nlp from './_lib.js'
 
 test('dmy option falsy', function (t) {
   t.equal('March 26', nlp('03/26').dates().format('{month} {date}').text(), 'WITHOUT dmy option')
+  t.equal('March 26', nlp('03/26').dates({dmy:false}).format('{month} {date}').text(), 'WITHOUT dmy option')
   t.equal('March 26', nlp('26/03').dates().format('{month} {date}').text(), 'WITHOUT dmy option')
+  t.equal('March 26', nlp('26/03').dates({dmy:false}).format('{month} {date}').text(), 'WITHOUT dmy option')
   t.equal('March 4', nlp('03/04').dates().format('{month} {date}').text(), 'WITHOUT dmy option')
+  t.equal('March 4', nlp('03/04').dates({dmy:false}).format('{month} {date}').text(), 'WITHOUT dmy option')
 
   t.end()
 })

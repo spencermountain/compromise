7fa4bb604f0480382d42eb2462dd420a56f4f06a
spencer kelly <spencerkelly86@gmail.com>
2026-05-23 10:49:51 -0400

    more tests

--- plugins/dates/tests/times.test.js
+++ plugins/dates/tests/times.test.js
@@ -69,6 +69,132 @@ test('times of start and end', function (t) {
     [`tues at 3pm to 5pm`, '3:00pm', '5:00pm'],
     [`june 4 at 2pm - 9pm`, '2:00pm', '9:00pm'],
     // [`june 4 at ten past 8`, '8:10pm'],
+['monday at 3', '3:00pm'],
+    ['monday at 3pm', '3:00pm'],
+    ['monday at 3:30', '3:30pm'],
+    ['monday at 3:30pm', '3:30pm'],
+    ['monday at 9am', '9:00am'],
+    ['monday at 9', '9:00am'],
+    ['friday at 5', '5:00pm'],
+    ['friday at 5pm', '5:00pm'],
+    ['thursday at noon', '12:00pm'],
+    ['thursday at midnight', '12:00am'],
+    ['today at 4', '4:00pm'],
+    ['today at 4pm', '4:00pm'],
+    ['tomorrow at 3', '3:00pm'],
+    ['tomorrow at 3pm', '3:00pm'],
+    ['yesterday at 2pm', '2:00pm'],
+    ['5:00 today', '5:00pm'],
+    ['5:30 in the morning', '5:30am'],
+    ['5 in the morning', '5:00am'],
+    ['5:30 in the afternoon', '5:30pm'],
+    ['6 in the evening', '6:00pm'],
+    ['7 at night', '7:00pm'],
+    ['noon today', '12:00pm'],
+    ['half past 4', '4:30pm'],
+    ['quarter past 3', '3:15pm'],
+    ['twenty past 2', '2:20pm'],
+    ['2 oclock pm', '2:00pm'],
+    ['2 oclock am', '2:00am'],
+    ['10 oclock', '10:00am'],
+    ['10 oclock pm', '10:00pm'],
+    ['between 9am and 12pm', '9:00am', '12:00pm'],
+    ['between 2pm and 4pm', '2:00pm', '4:00pm'],
+    ['between 2 and 4 tomorrow', '2:00pm', '4:00pm'],
+    ['between 3 and 6 monday', '3:00pm', '6:00pm'],
+    ['monday between 1 and 3pm', '1:00pm', '3:00pm'],
+    ['from 9am to 5pm', '9:00am', '5:00pm'],
+    ['from 2 to 4pm', '2:00pm', '4:00pm'],
+    ['monday from 9 to 5', '9:00am', '5:00pm'],
+    ['monday from 9am to 5pm', '9:00am', '5:00pm'],
+    ['monday from 2 to 4pm', '2:00pm', '4:00pm'],
+    ['9 to 5 monday', '9:00am', '5:00pm'],
+    ['9-5 monday', '9:00am', '5:00pm'],
+    ['9am-5pm monday', '9:00am', '5:00pm'],
+    ['mon 9 to 5', '9:00am', '5:00pm'],
+    ['mon from 9-5', '9:00am', '5:00pm'],
+    ['4pm to 6pm', '4:00pm', '6:00pm'],
+    ['4-6pm', '4:00pm', '6:00pm'],
+    ['4pm-6pm', '4:00pm', '6:00pm'],
+    ['3-4pm', '3:00pm', '4:00pm'],
+    ['11am to 1pm', '11:00am', '1:00pm'],
+    ['11-1pm', '11:00am', '1:00pm'],
+    ['4pm to 5pm today', '4:00pm', '5:00pm'],
+    ['4 to 5 tomorrow', '4:00pm', '5:00pm'],
+    ['between 4pm and 5pm tomorrow', '4:00pm', '5:00pm'],
+    ['friday from 3 to 5pm', '3:00pm', '5:00pm'],
+    ['fri from 3-5', '3:00pm', '5:00pm'],
+    ['thurs at 2pm to 4pm', '2:00pm', '4:00pm'],
+    ['wed between 1 and 3', '1:00pm', '3:00pm'],
+    ['wed between 1 - 3pm', '1:00pm', '3:00pm'],
+    ['march 5 from 2-4pm', '2:00pm', '4:00pm'],
+    ['march 5th 2pm to 4pm', '2:00pm', '4:00pm'],
+    ['2pm to 4pm on march 5', '2:00pm', '4:00pm'],
+    ['march 9 at 2pm - 5pm', '2:00pm', '5:00pm'],
+    ['january 5 from 11 to 1', '11:00am', '1:00pm'],
+    ['january 5th 11am to 1pm', '11:00am', '1:00pm'],
+    ['anytime after 5', '5:00pm'],
+    ['anytime after 5pm', '5:00pm'],
+    ['anytime after noon', '12:00pm'],
+    ['sometime after 3pm', '3:00pm'],
+    ['after 4pm', '4:00pm'],
+    ['in 2 hours', '12:05pm'],
+    ['in two hours', '12:05pm'],
+    ['in 30 minutes', '10:35am'],
+    ['next tuesday at 4', '4:00pm'],
+    ['next wednesday at 3pm', '3:00pm'],
+    ['this friday at 2pm', '2:00pm'],
+    ['@3:15', '3:15pm'],
+    ['@15:30', '3:30pm'],
+    ['@2:30', '2:30pm'],
+    ['@14:00', '2:00pm'],
+    ['8am to 4:30pm', '8:00am', '4:30pm'],
+    ['7:30am to 3:45pm', '7:30am', '3:45pm'],
+    ['between 8am and 4:30pm', '8:00am', '4:30pm'],
+    ['4pm or 6pm', '4:00pm'],
+    ['dinnertime', '6:00pm'],
+    ['lunchtime', '12:00pm'],
+    ['at 2pm', '2:00pm'],
+    ['at 2:30', '2:30pm'],
+    ['at 2:30pm', '2:30pm'],
+    ['around 3pm', '3:00pm'],
+    ['around 3:30', '3:30pm'],
+    ['1pm to 3pm', '1:00pm', '3:00pm'],
+    ['1-3pm', '1:00pm', '3:00pm'],
+    ['2:15pm to 4:45pm', '2:15pm', '4:45pm'],
+    ['9:05am to 5:55pm', '9:05am', '5:55pm'],
+    ['3-5 on march 2', '3:00pm', '5:00pm'],
+    ['february 1 from 9am-12pm', '9:00am', '12:00pm'],
+    ['between 10 and 2 tuesday', '10:00am', '2:00pm'],
+    ['tuesday between 10am and 2pm', '10:00am', '2:00pm'],
+    ['from 10 to 2 on tuesday', '10:00am', '2:00pm'],
+    ['friday from 4pm to 6', '4:00pm', '6:00pm'],
+    ['at 10 past 3', '3:10pm'],
+    ['at twenty past 4', '4:20pm'],
+    ['half past noon', '12:00pm'],
+    ['14:00', '2:00pm'],
+    ['14:30', '2:30pm'],
+    ['4pm est', '4:00pm'],
+    ['4pm eastern', '4:00pm'],
+    ['3pm pst', '3:00pm'],
+    ['sharp at 4pm', '4:00pm'],
+    ['4pm sharp', '4:00pm'],
+    ['4pm on the dot', '4:00pm'],
+    ['on the dot at 4pm', '4:00pm'],
+    ['tonight at 8', '8:00pm'],
+    ['tonight at 8pm', '8:00pm'],
+    ['8pm tonight', '8:00pm'],
+    ['at noon', '12:00pm'],
+    ['at midnight', '12:00am'],
+    ['around noon', '12:00pm'],
+    ['around 4pm', '4:00pm'],
+    ['sat at 3pm', '3:00pm'],
+    ['this morning at 9', '9:00am'],
+    ['wednesday from 2pm', '2:00pm'],
+    ['saturday from 10 to 12', '10:00am', '12:00pm'],
+    ['tuesday from 10 to 12', '10:00am', '12:00pm'],
+    ['2:30pm today', '2:30pm'],
+    ['2:30 today', '2:30pm'],
   ]
   arr.forEach((a) => {
     const doc = nlp(a[0])
@@ -107,6 +233,30 @@ test('times test', function (t) {
   doc = nlp('it was ten after 9')
   json = doc.times().json()[0]
   t.equal(json.time.time, '9:10am', 'ten after 9')
+
+  doc = nlp('around 3:30pm on tuesday')
+  json = doc.times({ today: '1996-03-28', timezone: 'Canada/Eastern' }).json()[0]
+  t.equal(json.time.time, '3:30pm', '3:30pm tuesday')
+
+  doc = nlp('at half past 3 on monday')
+  json = doc.times({ today: '1996-03-28', timezone: 'Canada/Eastern' }).json()[0]
+  t.equal(json.time.time, '3:30pm', 'half past 3 monday')
+
+  doc = nlp('at ten past 8')
+  json = doc.times().json()[0]
+  t.equal(json.time.time, '8:10am', 'ten past 8')
+
+  doc = nlp('at twenty past 4')
+  json = doc.times().json()[0]
+  t.equal(json.time.time, '4:20pm', 'twenty past 4')
+
+  doc = nlp('at 8:30 in the morning')
+  json = doc.times().json()[0]
+  t.equal(json.time.time, '8:30am', '8:30 morning')
+
+  doc = nlp('at 7 in the evening')
+  json = doc.times().json()[0]
+  t.equal(json.time.time, '7:00pm', '7 evening')
   t.end()
 })
 
@@ -118,5 +268,13 @@ test('times test', function (t) {
   doc = nlp('wednesday at 2:00am')
   doc.times().format('{hour-24-pad}:{minute-pad}')
   t.equal(doc.text(), 'wednesday at 02:00', here + 'fmt')
+
+  doc = nlp('today at 2:30pm')
+  doc.times().format('24h')
+  t.equal(doc.text(), 'today at 14:30', here + '24h today')
+
+  doc = nlp('friday at 6:45am')
+  doc.times().format('{hour-24-pad}:{minute-pad}')
+  t.equal(doc.text(), 'friday at 06:45', here + 'fmt friday')
   t.end()
 })

c4984cdae1eb0eab2c49ad3368b64f119075b0c2
DualFroz <me@dualfroz.com>
2026-09-14 16:17:27 +0200

    Skip the verb of a relative clause after the subject

--- tests/three/sentences/svo.test.js
+++ tests/three/sentences/svo.test.js
@@ -47,10 +47,11 @@ test('svo parser', function (t) {
     ],
     ['He bought the car that the dealer, the bank, and the broker all recommended.', 'he', 'bought'],
     ['I ate dinner before I went to bed.', 'i', 'ate'],
+    ['The man who called yesterday is my uncle.', 'the man', 'is'],
 
     // ['every day the kitten tries to eat the mouse', 'the kitten', 'tries to eat'],
-    // ['The boy who you saw at the store committed a robbery.', 'the boy', 'committed'],
-    // ['After dripping mustard all over his chest, the man who was wearing a red shirt wished that he had instead chosen ketchup for his hotdog.', 'the man', 'wished'],
+    ['The boy who you saw at the store committed a robbery.', 'the boy', 'committed'],
+    ['After dripping mustard all over his chest, the man who was wearing a red shirt wished that he had instead chosen ketchup for his hotdog.', 'the man', 'wished'],
     // ["please tell me you'll address the issue", 'you', 'will address'],
     // ['The store that the boy robbed is on the corner.', 'the store', 'is'],
   ]
